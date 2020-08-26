1284734d07f6e995b326fbc55a9f440a7fa66f68
Emma Pierce-Hoffman <epierceh@broadinstitute.org>
2020-08-26 12:21:07 -0400

    clean up and add warning of cached tasks

--- scripts/cromwell/analyze_resource_acquisition.py
+++ scripts/cromwell/analyze_resource_acquisition.py
@@ -13,6 +13,7 @@ import random
 import matplotlib.pyplot as plt
 import matplotlib.ticker
 import sys
+from os.path import basename
 SEED = 42
 
 """
@@ -25,18 +26,18 @@ Usage:
 Parameters:
 	workflow_metadata.json: path to Cromwell metadata file for workflow of interest
 	/path/to/output_basename: base output path, to which extensions will be appended for each output file, 
-		ie. /output_dir/basename will yield /output_dir/basename_plot.png, /output_dir/basename_table.tsv, etc
+		ie. /output_dir/basename will yield /output_dir/basename.plot.png, /output_dir/basename.table.tsv, etc
+		or, /output_dir/ will yield plot.png, table.tsv, etc
 
 Outputs:
 	- plot (png) of VMs, CPUs (total, preemptible, and nonpreemptible), RAM, and disk (HDD, SSD) acquisitioned over time 
 	- table of resource acquisition over time for each type of resource listed above
-	- text file containing peak resource acuqisition for each type of resource listed above
-	- [preemptible things]
-	- text file containing list of tasks that used call-caching
+	- text file of peak resource acquisition for each type of resource listed above
+	- (coming soon) [preemptible things]
+	- (prints to stdout) warning of any tasks that used call-caching
 """
-
-def get_seconds_from_epoch(time_string):
-	return (dateutil.parser.parse(time_string) - datetime.datetime(1900,1,1,tzinfo=tzutc())).total_seconds()
+NUM_CACHED = 0
+CACHED = set()
 
 def get_disk_info(metadata):
 	"""
@@ -74,7 +75,7 @@ def used_cached_results(metadata):
 	"""
 	Source: https://github.com/broadinstitute/dsde-pipelines/blob/develop/scripts/calculate_cost.py
 	"""
-	return "callCaching" in metadata and metadata["callCaching"]["hit"]
+	return "callCaching" in metadata and "hit" in metadata["callCaching"] and metadata["callCaching"]["hit"]
 
 def calculate_start_end(call_info):
 	"""
@@ -119,16 +120,16 @@ def was_preempted(call_info):
 	# We treat Preempted and RetryableFailure the same.  The latter is a general case of the former
 	return call_info['executionStatus'] in ['Preempted', 'RetryableFailure']
 
-def calculate_start_end_simple(m):
-	start = 'na'
-	if 'start' in m:
-		start = get_seconds_from_epoch(m['start'])
-
-	end = 'na'
-	if 'end' in m:
-		end = get_seconds_from_epoch(m['end'])
-
-	return start, end
+def get_mem_cpu(m):
+	cpu = 'na'
+	memory = 'na'
+	if 'runtimeAttributes' in m:
+		if 'cpu' in m['runtimeAttributes']:
+			cpu = int(m['runtimeAttributes']['cpu'])
+		if 'memory' in m['runtimeAttributes']:
+			mem_str = m['runtimeAttributes']['memory']
+			memory = float(mem_str[:mem_str.index(" ")])
+	return cpu, memory
 
 def getCalls(m, alias=None):
 	"""
@@ -161,16 +162,14 @@ def getCalls(m, alias=None):
 
 	start, end = calculate_start_end(m)
 
-	cpu = 'na'
-	memory = 'na'
-	if 'runtimeAttributes' in m:
-		if 'cpu' in m['runtimeAttributes']:
-			cpu = int(m['runtimeAttributes']['cpu'])
-		if 'memory' in m['runtimeAttributes']:
-			mem_str = m['runtimeAttributes']['memory']
-			memory = float(mem_str[:mem_str.index(" ")])
+	cpu, memory = get_mem_cpu(m)
 
 	cached = used_cached_results(m)
+	if cached:
+		global CACHED
+		global NUM_CACHED
+		CACHED.add(alias)
+		NUM_CACHED += 1
 
 	callroot = 'na'
 	if 'callRoot' in m:
@@ -200,30 +199,32 @@ def getCalls(m, alias=None):
 
 	# in a call
 	if alias and ('monitoringLog' in m):
-		#call_metadata.append((alias, start,end,shard_index, attempt, job_id, cpu, cached, callroot, memory, preemptible, hdd_size, ssd_size))
 		call_metadata.append((start, 1, cpu, preemptible_cpu, nonpreemptible_cpu, memory, hdd_size, ssd_size))
 		call_metadata.append((end, -1, -1*cpu, -1*preemptible_cpu, -1*nonpreemptible_cpu, -1*memory, -1*hdd_size, -1*ssd_size))
 
 	return call_metadata
 
-def get_data_table(metadata_file):
+def get_call_metadata(metadata_file):
 	"""
-	Based on: 
-		https://github.com/broadinstitute/dsde-pipelines/blob/master/scripts/quota_usage.py
-		https://github.com/broadinstitute/gatk-sv/blob/master/scripts/cromwell/download_monitoring_logs.py
+	Based on: https://github.com/broadinstitute/gatk-sv/blob/master/scripts/cromwell/download_monitoring_logs.py
 	"""
 	metadata = json.load(open(metadata_file, 'r'))
 	colnames = ['timestamp', 'vm_delta', 'cpu_all_delta', 'cpu_preemptible_delta', 'cpu_nonpreemptible_delta', 'memory_delta', 'hdd_delta', 'ssd_delta']
 
 	call_metadata = getCalls(metadata, metadata['workflowName'])
 	call_metadata = pd.DataFrame(call_metadata, columns=colnames)
-	#call_metadata[['timestamp']] = call_metadata[['timestamp']].astype('datetime64')
 
+	return call_metadata
+
+def transform_call_metadata(call_metadata):
+	"""
+	Based on: https://github.com/broadinstitute/dsde-pipelines/blob/master/scripts/quota_usage.py
+	"""
 	call_metadata = call_metadata.sort_values(by='timestamp')
 	call_metadata['timestamp'] -= call_metadata.timestamp.iloc[0] # make timestamps start from 0 by subtracting minimum (at index 0 after sorting)
 	# print(call_metadata[['timestamp']])
-	call_metadata['seconds'] = call_metadata['timestamp'].dt.total_seconds()
-	print(call_metadata.dtypes)
+	call_metadata['seconds'] = call_metadata['timestamp'].dt.total_seconds() # get timedelta in seconds because plot labels won't format correctly otherwise
+	#print(call_metadata.dtypes)
 	
 	call_metadata['vm'] = call_metadata.vm_delta.cumsum()
 	call_metadata['cpu_all'] = call_metadata.cpu_all_delta.cumsum()
@@ -235,20 +236,19 @@ def get_data_table(metadata_file):
 
 	return call_metadata
 
-def plot(df, title_name, output_name):
+def plot_resources_time(df, title_name, output_name):
 	"""
 	Modified from: https://github.com/broadinstitute/dsde-pipelines/blob/master/scripts/quota_usage.py
 	"""
 	print("Writing " + output_name, file = sys.stderr)
 	colors = {
-		"vm": "#006FA6", #blue (dark blue, could use if change to turquoise/cyan: "#0000A6")
+		"vm": "#006FA6", #blue 
 		"cpu_all": "black",
 		"cpu_preemptible": "#10a197", #turquoise 
 		"cpu_nonpreemptible": "#A30059", #dark pink
 		"memory": "#FF4A46", #coral red
 		"hdd": "#72418F", #purple
 		"ssd": "#008941", #green
-		#  "#FF913F", # orange # "gray"
 	}
 	LABEL_SIZE = 17
 	TITLE_SIZE = 20
@@ -285,14 +285,25 @@ def plot(df, title_name, output_name):
 	
 	fig.savefig(output_name, bbox_inches='tight')
 
-def print_peak_usage(m):
-	print("Peak VMs: " + str(max(m['vm'])))
-	print("Peak CPUs (all): " + str(max(m['cpu_all'])))
-	print("Peak Preemptible CPUs: " + str(max(m['cpu_preemptible'])))
-	print("Peak Non-preemptible CPUs: " + str(max(m['cpu_nonpreemptible'])))
-	print("Peak RAM: " + "{:.2f}".format(max(m['memory'])) + " GiB")
-	print("Peak HDD: " + str(max(m['hdd'])) + " GiB")
-	print("Peak SSD: " + str(max(m['ssd'])) + " GiB")
+def write_resources_time_table(call_metadata, table_file):
+	print("Writing " + table_file, file = sys.stderr)
+	call_metadata.to_csv(
+		table_file, 
+		columns = ["timestamp", "seconds", "vm", "cpu_all", "cpu_preemptible", "cpu_nonpreemptible", "memory", "hdd", "ssd"],
+		sep='\t', 
+		index = False
+	)
+
+def write_peak_usage(m, peak_file):
+	print("Writing " + peak_file, file = sys.stderr)
+	with open(peak_file, 'w') as out:
+		out.write("Peak VMs: " + str(max(m['vm'])) + "\n")
+		out.write("Peak CPUs (all): " + str(max(m['cpu_all'])) + "\n")
+		out.write("Peak Preemptible CPUs: " + str(max(m['cpu_preemptible'])) + "\n")
+		out.write("Peak Non-preemptible CPUs: " + str(max(m['cpu_nonpreemptible'])) + "\n")
+		out.write("Peak RAM: " + "{:.2f}".format(max(m['memory'])) + " GiB" + "\n")
+		out.write("Peak HDD: " + str(max(m['hdd'])) + " GiB" + "\n")
+		out.write("Peak SSD: " + str(max(m['ssd'])) + " GiB" + "\n")
 
 # Main function
 def main():
@@ -308,17 +319,32 @@ def main():
 	plt_title = args.plot_title
 	if plt_title != "":
 		plt_title += " "
+	sep = "."
+	if basename(output_base) == "":
+		sep = ""
 
-	call_metadata = get_data_table(metadata_file)
+	call_metadata = get_call_metadata(metadata_file)
+	call_metadata = transform_call_metadata(call_metadata)
+
+	global CACHED
+	global NUM_CACHED
+	if NUM_CACHED > 0:
+		if NUM_CACHED == 1:
+			print("Warning: %d task was call-cached, so this analysis may underestimate typical resource acquisition for this workflow. \nThe cached task was:" % NUM_CACHED)
+		else:
+			print("Warning: %d tasks were call-cached, so this analysis may underestimate typical resource acquisition for this workflow. \nCached tasks include (repeat names omitted):" % NUM_CACHED)
+		print(", ".join(list(CACHED)))
+	else:
+		print("No cached tasks")
 	
-	plot_file = output_base + ".plot.png"
-	plot(call_metadata, plt_title, plot_file)
+	plot_file = output_base + sep + "plot.png"
+	plot_resources_time(call_metadata, plt_title, plot_file)
 
-	table_file = output_base + ".table.tsv"
-	print("Writing " + table_file)
-	call_metadata.to_csv(table_file, sep='\t', index = False)
+	table_file = output_base + sep + "table.tsv"
+	write_resources_time_table(call_metadata, table_file)
 
-	print_peak_usage(call_metadata)
+	peak_file = output_base + sep + "peaks.txt"
+	write_peak_usage(call_metadata, peak_file)
 
 
 if __name__== "__main__":
