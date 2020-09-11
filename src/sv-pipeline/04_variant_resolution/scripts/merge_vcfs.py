300023f147b1fe6170e55584dfbecf4b952d0c06
Emma Pierce-Hoffman <epierceh@broadinstitute.org>
2020-09-11 18:06:42 -0400

    revert to previous de-dup method, add comment to explain, group only if chrom matches, and re-sort on CHR2/END2

--- src/sv-pipeline/04_variant_resolution/scripts/merge_vcfs.py
+++ src/sv-pipeline/04_variant_resolution/scripts/merge_vcfs.py
@@ -5,7 +5,6 @@
 # Distributed under terms of the MIT license.
 
 """
-
 """
 
 import argparse
@@ -15,100 +14,80 @@ import pysam
 import svtk.utils as svu
 
 
-# def records_match(record, other):
-#     """Test if two records are same SV: check chromosome, position, stop, SVTYPE, and (if they exist) CHR2/END2 for multi-chromosomal events"""
-#     return (record.chrom == other.chrom and
-#             (('CHR2' not in record.info and 'CHR2' not in other.info) or ('CHR2' in record.info and 'CHR2' in other.info and record.info['CHR2'] == other.info['CHR2'])) and
-#             record.pos == other.pos and
-#             record.stop == other.stop and
-#             (('END2' not in record.info and 'END2' not in other.info) or ('END2' in record.info and 'END2' in other.info and record.info['END2'] == other.info['END2'])) and
-#             record.info['SVTYPE'] == other.info['SVTYPE'])
+def records_match(record, other):
+    """Test if two records are same SV: check chromosome, position, stop, SVTYPE, and (if they exist) CHR2/END2 for multi-chromosomal events"""
+    return (record.chrom == other.chrom and
+            record.pos == other.pos and
+            record.stop == other.stop and
+            record.info['SVTYPE'] == other.info['SVTYPE'] and
+            (('CHR2' not in record.info and 'CHR2' not in other.info) or ('CHR2' in record.info and 'CHR2' in other.info and record.info['CHR2'] == other.info['CHR2'])) and
+            (('END2' not in record.info and 'END2' not in other.info) or ('END2' in record.info and 'END2' in other.info and record.info['END2'] == other.info['END2'])))
 
 
-# def merge_key(record):
-#     """Sort records by coordinate then svtype"""
-#     return (record.pos, record.stop, record.info['SVTYPE'], record.id)
+def merge_key(record):
+    """Sort records by coordinate then svtype"""
+    chr2 = record.info['CHR2'] if 'CHR2' in record.info else None
+    end2 = record.info['END2'] if 'END2' in record.info else None
+    return (record.pos, record.stop, record.info['SVTYPE'], chr2, end2, record.id)
 
 
-# def dedup_records(records):
-#     """Take unique subset of records"""
+def dedup_records(records):
+    """Take unique subset of records"""
 
-#     records = sorted(records, key=merge_key)
+    records = sorted(records, key=merge_key)
 
-#     curr_record = records[0]
-#     for record in records[1:]:
-#         if records_match(curr_record, record):
-#             print("Match!: ", end="") # debugging
-#             print(curr_record, end=", ")
-#             print(record)
-#             continue
-#         else:
-#             yield curr_record
-#             curr_record = record
-
-#     yield curr_record
-
-
-class VariantRecordComparison:
-    def __init__(self, record):
-        self.pos = record.pos
-        self.stop = record.stop
-        self.svtype = record.info['SVTYPE']
-        self.id = record.id
-        self.chrom = record.chrom
-        self.chr2 = record.info['CHR2'] if 'CHR2' in record.info else None
-        self.end2 = record.info['END2'] if 'END2' in record.info else None
-
-
-    def __lt__(self, other):
-        if self.chrom == other.chrom:
-            return (self.pos, self.stop, self.svtype, self.chr2, self.end2, self.id) < (other.pos, other.stop, other.svtype, other.chr2, other.end2, other.id)
-            # return (self.pos, self.stop, self.svtype, self.id) < (other.pos, other.stop, other.svtype, other.id)
-        else:
-            return svu.is_smaller_chrom(self.chrom, other.chrom)
-
-    def __gt__(self, other):
-        if self.chrom == other.chrom:
-            return (self.pos, self.stop, self.svtype, self.chr2, self.end2, self.id) > (other.pos, other.stop, other.svtype, other.chr2, other.end2, other.id)
-            # return (self.pos, self.stop, self.svtype, self.id) < (other.pos, other.stop, other.svtype, other.id)
-        else:
-            return (not svu.is_smaller_chrom(self.chrom, other.chrom))
-
-    def __le__(self, other):
-        return (((self.chrom, self.pos, self.stop, self.chr2, self.end2, self.svtype) == (other.chrom, other.pos, other.stop, other.chr2, other.end2, other.svtype)) or
-            svu.is_smaller_chrom(self.chrom, other.chrom) or
-            (self.pos, self.stop, self.svtype, self.chr2, self.end2, self.id) < (other.pos, other.stop, other.svtype, other.chr2, other.end2, other.id))
-
-    def __ge__(self, other):
-        return (((self.chrom, self.pos, self.stop, self.chr2, self.end2, self.svtype) == (other.chrom, other.pos, other.stop, other.chr2, other.end2, other.svtype)) or
-            (not svu.is_smaller_chrom(self.chrom, other.chrom)) or
-            (self.pos, self.stop, self.svtype, self.chr2, self.end2, self.id) > (other.pos, other.stop, other.svtype, other.chr2, other.end2, other.id))
-
-    def __eq__(self, other):
-        return (self.chrom, self.pos, self.stop, self.chr2, self.end2, self.svtype) == (other.chrom, other.pos, other.stop, other.chr2, other.end2, other.svtype)
-
-def merge_records(vcfs):
-    """
-    Take unique set of VCFs
-    """
-
-    merged_vcfs = heapq.merge(*vcfs, key=lambda r: VariantRecordComparison(r))
-
-    record = next(merged_vcfs)
-    curr_record = record
-
-    for record in merged_vcfs:
-        # if records_match(curr_record, record):
-        if VariantRecordComparison(curr_record) == VariantRecordComparison(record):
-            print("Match!: ", end="") # debugging
-            print(curr_record, end=", ")
-            print(record)
+    curr_record = records[0]
+    for record in records[1:]:
+        if records_match(curr_record, record):
             continue
         else:
             yield curr_record
             curr_record = record
 
     yield curr_record
+
+
+class VariantRecordComparison:
+    def __init__(self, record):
+        self.record = record
+
+    def __lt__(self, other):
+        if self.record.chrom == other.record.chrom:
+            return self.record.pos < other.record.pos
+        else:
+            return svu.is_smaller_chrom(self.record.chrom, other.record.chrom)
+
+
+def merge_records(vcfs):
+    """
+    Take unique set of VCF records
+    Strategy: Merge & roughly sort records from all VCFs by chrom & pos, then gather records that share the same chrom & pos and remove duplicates.
+    Note: The output from heapq.merge cannot be directly used to remove duplicates because it is not sufficiently sorted, so duplicates may not be 
+        adjacent. It is also not sufficient to alter the comparator function to take more than chrom & pos into account, because heapq.merge assumes 
+        that each VCF is already sorted and will make no attempt to further sort them according to the comparator function. Re-sorting all records
+        that share a chrom & pos is more efficient than re-sorting each entire VCF.
+    """
+
+    merged_vcfs = heapq.merge(*vcfs, key=lambda r: VariantRecordComparison(r))
+
+    record = next(merged_vcfs)
+    curr_records = deque([record])
+    curr_chrom = record.chrom
+    curr_pos = record.pos
+
+    for record in merged_vcfs:
+        if record.chrom == curr_chrom and record.pos == curr_pos:
+            curr_records.append(record)
+        else:
+            for rec in dedup_records(curr_records):
+                yield rec
+
+            curr_records = deque([record])
+            curr_pos = record.pos
+            curr_chrom = record.chrom
+
+    for rec in dedup_records(curr_records):
+        yield rec
 
 
 def main():
