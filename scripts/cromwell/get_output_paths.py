d4c40964bfe516a33c661251478cb65be8131828
Emma Pierce-Hoffman <epierceh@broadinstitute.org>
2021-07-01 12:39:37 -0400

    second pass at PR comments

--- scripts/cromwell/get_output_paths.py
+++ scripts/cromwell/get_output_paths.py
@@ -4,7 +4,8 @@ import argparse
 import json
 import logging
 import re
-from os.path import isfile, getsize
+import os.path
+from urllib.parse import urlparse
 
 from google.cloud import storage
 
@@ -53,13 +54,15 @@ Author: Emma Pierce-Hoffman (epierceh@broadinstitute.org)
 
 
 def check_file_nonempty(f):
-    if not isfile(f):
+    # Validate existence of file and that it is > 0 bytes
+    if not os.path.isfile(f):
         raise RuntimeError("Required input file %s does not exist." % f)
-    elif getsize(f) == 0:
+    elif os.path.getsize(f) == 0:
         raise RuntimeError("Required input file %s is empty." % f)
 
 
 def read_entities_file(entities_file):
+    # Get list of entities from -e entities file
     entities = []
     if entities_file is not None:
         # proceed with reading file - must not be None at this point
@@ -71,44 +74,54 @@ def read_entities_file(entities_file):
 
 
 def load_filenames(filenames):
+    # Read -f filenames / output names JSON
     files_dict = json.load(open(filenames, 'r'))
     output_names = sorted(files_dict.keys())
     return files_dict, output_names
 
 
 def split_bucket_subdir(directory):
-    regex = r'^(gs://)?([^/]+)(/)?(.*)'
-    bucket = re.match(regex, directory).group(2)
-    subdir = re.match(regex, directory).group(4)
-    if subdir[-1] != '/':
-        subdir += "/"
-    return bucket, subdir
+    # Parse -b URI input into top-level bucket name (no gs://) and subdirectory path
+    uri = urlparse(directory)
+    return uri.netloc, uri.path.lstrip("/")
 
 
 def get_batch_dirs(workflows, workflow_id, directory):
+    # Return list of (batch_name, batch_subdirectory) and top-level bucket parsed from -b URI input
     batches_dirs = []  # to hold tuples of (batch, dir) in order given in input
     bucket, subdir = split_bucket_subdir(directory)
+    # If using -i input, just add workflow ID to subdirectory path and return
     if workflow_id is not None:
-        return [("placeholder_batch", subdir + workflow_id + "/")], bucket
+        return [("placeholder_batch", os.path.join(subdir, workflow_id))], bucket
+    # If using -w input, read workflows file to get batch names and workflow IDs
     with open(workflows, 'r') as inp:
         for line in inp:
             if line.strip() == "":
                 continue
             (batch, workflow) = line.strip().split('\t')
-            batch_dir = subdir + workflow + "/"
+            batch_dir = os.path.join(subdir, workflow)
             batches_dirs.append((batch, batch_dir))
     return batches_dirs, bucket
 
 
-def file_search(blobs, names_left, files_dict, num_found, bucket, output_names):
-    # go through each object in bucket once, checking if it matches any filenames not yet found
+def find_batch_output_files(batch, bucket, prefix, files_dict, output_names, num_outputs):
+    # Search batch directory for files with specified prefixes
+
+    # Get all objects in directory
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket, prefix=prefix,
+                                      delimiter=None)  # only one workflow per batch - assumes caching if multiple
+
+    # Go through each object in directory once, checking if it matches any filenames not yet found
     batch_outputs = {file: None for file in output_names}
+    names_left = list(output_names)
+    num_found = 0
     for blob in blobs:
         blob_name = blob.name.strip()
         # in case multiple files, continue matching on suffixes even if already found file match(es)
         for name in output_names:
             if blob_name.endswith(files_dict[name]):
-                blob_path = "gs://" + bucket + "/" + blob_name  # reconstruct URI
+                blob_path = os.path.join("gs://", bucket, blob_name)  # reconstruct URI
                 if batch_outputs[name] is None:
                     num_found += 1
                     names_left.remove(name)
@@ -116,27 +129,18 @@ def file_search(blobs, names_left, files_dict, num_found, bucket, output_names):
                 else:
                     batch_outputs[name].append(blob_path)
                 break
-    return batch_outputs, num_found, names_left
 
-
-def find_batch_output_files(batch, bucket, prefix, files_dict, output_names, num_outputs):
-    num_found = 0
-    storage_client = storage.Client()
-    blobs = storage_client.list_blobs(bucket, prefix=prefix,
-                                      delimiter=None)  # only one workflow per batch - assumes caching if multiple
-    names_left = list(output_names)
-
-    batch_outputs, num_found, names_left = file_search(blobs, names_left, files_dict,
-                                                       num_found, bucket, output_names)
-    # warn if some outputs not found
+    # Warn if some outputs not found
     if num_found < num_outputs:
         for name in names_left:
             logging.warning(f"{batch} output file {name} not found in gs://{bucket}/{prefix}. Outputting empty string")
             batch_outputs[name] = ""
+
     return batch_outputs
 
 
 def sort_files_by_shard(file_list):
+    # Attempt to sort file list by shard number based on last occurrence of "shard-" in URI
     regex = r'^(shard-)([0-9]+)(/.*)'  # extract shard number for sorting - group 2
     shard_numbers = []
     check_different_shard = None
@@ -157,63 +161,60 @@ def sort_files_by_shard(file_list):
 
 
 def format_batch_line(batch, output_names, batch_outputs):
+    # Format line with batch and outputs (if not using entities option)
     batch_line = batch
     for name in output_names:
         file_list = batch_outputs[name]
+        # If could not find a file, make sure to add an offset tab to create an empty entry
         if file_list == "":
             batch_line += "\t"
+        # Single File output
         elif len(file_list) == 1:
             batch_line += "\t" + file_list[0]
+        # Array[File] output - attempt sorting by shard
         else:
             batch_line += '\t["' + '", "'.join(sort_files_by_shard(file_list)) + '"]'
     batch_line += "\n"
     return batch_line
 
 
-def get_entity_outputs(output_names, batch_outputs, entities, entity_outputs):
-    # edit entity_outputs dict in place
-    for name in output_names:
+def update_entity_outputs(output_names, batch_outputs, entities, entity_outputs):
+    # Edit entity_outputs dict in place: add new batch outputs to each corresponding entity
+    for output_index, name in enumerate(output_names):
         filepaths = batch_outputs[name]
         filenames = [path.split("/")[-1] for path in filepaths]
         for entity in entities:  # not efficient but should be <500 entities and filenames to search
-            found = False
             for i, filename in enumerate(filenames):
-                if entity in filename:
+                # cannot handle Array[File] output for one entity
+                if entity in filename and entity_outputs[entity][output_index] == "":
+                    entity_outputs[entity][output_index] = filepaths[i]
                     entity_outputs[entity].append(filepaths[i])
                     filenames.remove(filename)
                     filepaths.remove(filepaths[i])
-                    found = True
                     break
-            if not found:
-                # logging.warning(f"{entity} output file {name} not found in provided directories. "
-                #                 f"Outputting empty string")
-                entity_outputs[entity].append("")
-    entities_left = []
+
+
+def write_entity_outputs(entity_outputs, keep_all_entities, entities, output_stream):
+    # Check, format, and write entity outputs
+    # do write inside function to be able to print line-by-line
     for entity in entities:
+        # check for blank entities
         if all(element == "" for element in entity_outputs[entity]):
-            entity_outputs[entity] = []
-            entities_left.append(entity)
-    return entities_left
-
-
-def format_entity_outputs(entity_outputs, keep_all_entities, entities):
-    output_string = ""
-    for entity in entities:
-        if not keep_all_entities and all(element == "" for element in entity_outputs[entity]):
-            logging.info(f"No output files found for {entity} in provided directories. Omitting from output")
-            continue
-        output_string += entity + "\t" + "\t".join(entity_outputs[entity]) + "\n"
-    return output_string
+            if keep_all_entities:
+                logging.info(f"No output files found for entity '{entity}' in provided directories. "
+                             f"Outputting blank entry. Remove -k argument to exclude empty entities.")
+            else:
+                logging.info(f"No output files found for entity '{entity}' in provided directories. "
+                             f"Omitting from output. Use -k argument to include empty entities.")
+                continue
+        output_stream.write(entity + "\t" + "\t".join(entity_outputs[entity]) + "\n")
 
 
 def retrieve_and_write_output_files(batches_dirs, bucket, files_dict, output_names, output_file,
                                     entities, entity_type, keep_all_entities):
     num_outputs = len(output_names)
     num_entities = len(entities)
-    entity_outputs = None
-    if num_entities > 0:
-        entities_left = list(entities)
-        entity_outputs = {entity: [] for entity in entities}
+    entity_outputs = {entity: [""] * num_outputs for entity in entities}  # empty if entities is empty
     logging.info("Writing %s" % output_file)
     with open(output_file, 'w') as out:
         out.write(entity_type + "\t" + "\t".join(output_names) + "\n")
@@ -221,12 +222,12 @@ def retrieve_and_write_output_files(batches_dirs, bucket, files_dict, output_nam
             logging.info("Searching for outputs for %s" % batch)
             batch_outputs = find_batch_output_files(batch, bucket, batch_dir, files_dict, output_names, num_outputs)
             if num_entities > 0:
-                entities_left = get_entity_outputs(output_names, batch_outputs, entities_left, entity_outputs)
+                update_entity_outputs(output_names, batch_outputs, entities, entity_outputs)
             else:
                 batch_line = format_batch_line(batch, output_names, batch_outputs)
                 out.write(batch_line)
         if num_entities > 0:
-            out.write(format_entity_outputs(entity_outputs, keep_all_entities, entities))
+            write_entity_outputs(entity_outputs, keep_all_entities, entities, out)
     logging.info("Done!")
 
 
@@ -246,7 +247,7 @@ def main():
     parser.add_argument("-o", "--output-file", required=True, help="Output file path")
     parser.add_argument("-b", "--bucket", required=True,
                         help="Google bucket path to search for files - should include all subdirectories "
-                             "preceding workflow ID")
+                             "preceding workflow ID, including the workflow name.")
     parser.add_argument("-l", "--log-level", required=False, default="INFO",
                         help="Specify level of logging information, ie. info, warning, error (not case-sensitive). "
                              "Default: INFO")
@@ -265,24 +266,29 @@ def main():
                              "output files are found.")
     args = parser.parse_args()
 
+    # Set logging level from -l input
     log_level = args.log_level
     numeric_level = getattr(logging, log_level.upper(), None)
     if not isinstance(numeric_level, int):
         raise ValueError('Invalid log level: %s' % log_level)
     logging.basicConfig(level=numeric_level, format='%(levelname)s: %(message)s')
 
+    # Set required arguments. Validate existence of & read filenames JSON
     filenames, output_file, bucket = args.filenames, args.output_file, args.bucket  # required
     check_file_nonempty(filenames)
     files_dict, output_names = load_filenames(filenames)
 
+    # Determine workflow IDs from -w or -i arguments. Get subdirectories
     workflows, workflow_id = args.workflows_file, args.workflow_id
     if workflows is not None:
         check_file_nonempty(workflows)
     batches_dirs, bucket = get_batch_dirs(workflows, workflow_id, bucket)
 
+    # Set entity arguments and read entities file
     entity_type, entities_file, keep_all_entities = args.entity_type, args.entities_file, args.keep_all_entities
     entities = read_entities_file(entities_file)
 
+    # Core functionality
     retrieve_and_write_output_files(batches_dirs, bucket, files_dict, output_names, output_file,
                                     entities, entity_type, keep_all_entities)
 
