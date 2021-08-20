350a2282c0c8bda8a051201a8491ddd264615ef7
vjalili <VJalili@users.noreply.github.com>
2021-08-20 14:32:18 -0700

    Remove temp code.

--- wdl_test/carrot_helper.py
+++ wdl_test/carrot_helper.py
@@ -333,7 +333,7 @@ class CarrotHelper:
         """
         name = name or "gatk-sv"
         if timestamp:
-            name = f"{name}_{get_timestamp()}"
+            name = f"{name}_{self.get_timestamp()}"
 
         c = f"carrot_cli -q {command} {subcommand} "
         if add_name:
@@ -756,75 +756,6 @@ class RunStatus:
     latest_status: str
     test_cromwell_job_id: str
     eval_cromwell_job_id: str
-
-
-def get_timestamp():
-    return calendar.timegm(time.gmtime())
-
-
-def response_to_json(response):
-    return json.loads(response.decode("utf8").replace("\'", "\"").replace("None", "\"None\""))
-
-
-def call_carrot_cli(command):
-    # process = Popen(command.split(" "), stdout=subprocess.PIPE)
-    # data, err = process.communicate()
-    try:
-        data = check_output(command, shell=True)
-        # return response_to_json(data) if process.returncode == 0 else err
-        return response_to_json(data)
-    except subprocess.CalledProcessError as grepexc:
-        print("error code", grepexc.returncode, grepexc.output)
-
-
-def create_test():
-    pipeline_id = "2214f31b-8aee-48cd-a81f-a0bd3091fa54"
-    test_wdl = "https://raw.githubusercontent.com/VJalili/gatk-sv/ehdn_unit_test/wdl/ExpansionHunterDenovo.wdl"
-    eval_wdl = "https://raw.githubusercontent.com/VJalili/gatk-sv/ehdn_unit_test/wdl_test/ExpansionHunterDenovo/eval_casecontrol_locus.wdl"
-    report_id = "ae61fbff-4270-4528-91a4-1c0091076289"
-    test_input_defaults = "wdl_test/ExpansionHunterDenovo/test_casecontrol_locus_default_inputs.json"
-    eval_input_defaults = "wdl_test/ExpansionHunterDenovo/eval_casecontrol_locus_default_inputs.json"
-    results = {
-        "EHdnSTRAnalysis.multisample_profile": "b4a2bdf7-a6b1-479d-82a5-53f63ac9861c",
-        "EvalCaseControlLocus.data_file": "3ecb2895-50a0-40d0-8931-6d8d6cfd2b5c"
-    }
-
-    cmd = f"carrot_cli template create --pipeline_id {pipeline_id} --name test_{get_timestamp()} --test_wdl {test_wdl} --eval_wdl {eval_wdl}"
-    output = call_carrot_cli(cmd)
-    template_id = output["template_id"]
-    print(f"\nCreated Template:\n{output}\n")
-
-    for workflow_output_label, result_id in results.items():
-        cmd = f"carrot_cli template map_to_result {template_id} {result_id} {workflow_output_label}"
-        output = call_carrot_cli(cmd)
-        print(f"\nCreated Result:\n{output}\n")
-
-    cmd = f"carrot_cli template map_to_report {template_id} {report_id}"
-    output = call_carrot_cli(cmd)
-    print(f"\nCreated Template mapping to Report:\n{output}\n")
-
-    cmd = f"carrot_cli test create --name test_{get_timestamp()} --template_id {template_id} --test_input_defaults {test_input_defaults} --eval_input_defaults {eval_input_defaults}"
-    output = call_carrot_cli(cmd)
-    print(f"\nCreated Test:\n{output}\n")
-    return output["test_id"]
-
-
-def run_test(test_id):
-    test_input = "wdl_test/ExpansionHunterDenovo/test_casecontrol_locus_inputs.json"
-    eval_input = "wdl_test/ExpansionHunterDenovo/eval_casecontrol_locus_inputs.json"
-    cmd = f"carrot_cli test run --test_input {test_input} --eval_input {eval_input} {test_id}"
-    output = call_carrot_cli(cmd)
-    print(f"\nExecuted Test:\n{output}\n")
-    print(f"\n\nRun ID: {output['run_id']}")
-    print(f"Get Run Status: carrot_cli run find_by_id {output['run_id']}")
-
-
-def delete_all_software_created_by(email="jalili.vahid@broadinstitute.org"):
-    command = f"carrot_cli software find --created_by {email}"
-    response = check_output(command, shell=True)
-    my_softwares = json.loads(response.decode("utf8").replace("\'", "\"").replace("None", "\"None\""))
-    for x in my_softwares:
-        response = check_output(command, shell=True)
 
 
 def is_url_accessible(url):
