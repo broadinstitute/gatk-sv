1aa371911869b8e687a52390abfc4b68a479f6bb
Chris Whelan <cwhelan@broadinstitute.org>
2021-09-16 11:56:41 -0400

    address differences in redundancy algorithm

--- src/sv-pipeline/04_variant_resolution/scripts/clean_vcf_part5_find_redundant_multiallelics.py
+++ src/sv-pipeline/04_variant_resolution/scripts/clean_vcf_part5_find_redundant_multiallelics.py
@@ -5,6 +5,16 @@ import sys
 import svtk.utils as svu
 
 
+def process_features_for_size1(features_for_size1, redundant_multiallelics):
+    for intersection in sorted(features_for_size1, key=lambda x: int(x[9]) - int(x[8]), reverse=True):
+        b_len = int(intersection.fields[9]) - int(intersection.fields[8])
+        overlap = int(intersection.fields[14])
+        small_coverage = overlap / b_len
+        if small_coverage > 0.50:
+            if intersection.fields[3] not in redundant_multiallelics:
+                redundant_multiallelics.add(intersection.fields[10])
+
+
 def main():
     parser = argparse.ArgumentParser(
         description=__doc__,
@@ -18,17 +28,21 @@ def main():
 
     redundant_multiallelics = set()
     self_inter = multiallelic_bed.intersect(multiallelic_bed, wo=True)\
-        .filter(lambda feature: feature[3] != feature[10])
+        .filter(lambda feature: feature[3] != feature[10]) \
+        .filter(lambda feature: (int(feature[2]) - int(feature[1])) >= (int(feature[9]) - int(feature[8]))) \
+        .sort(sizeD=True)
+    current_size1 = -1
+    features_for_size1 = []
     for feature in self_inter:
-        a_len = int(feature.fields[2]) - int(feature.fields[1])
-        b_len = int(feature.fields[9]) - int(feature.fields[8])
-        overlap = int(feature.fields[14])
-        small_coverage = overlap / min(a_len, b_len)
-        if small_coverage > 0.50:
-            if a_len < b_len:
-                redundant_multiallelics.add(feature.fields[3])
-            else:
-                redundant_multiallelics.add(feature.fields[10])
+        size1 = int(feature[2]) - int(feature[1])
+        if size1 != current_size1:
+            process_features_for_size1(features_for_size1, redundant_multiallelics)
+            features_for_size1 = []
+
+        current_size1 = size1
+        features_for_size1.append(feature)
+
+    process_features_for_size1(features_for_size1, redundant_multiallelics)
     print("identified {} redundant multiallelic sites".format(len(redundant_multiallelics)), file=sys.stderr)
     with open(args.fout, "w") as list_file:
         for vid in redundant_multiallelics:
