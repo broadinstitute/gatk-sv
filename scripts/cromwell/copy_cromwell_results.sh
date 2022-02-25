1639ff892033a20857abab13512da7c209126529
Ted Brookings <tbrookin@broadinstitute.org>
2022-02-25 14:08:10 -0500

    Add GqRecalibrator wdls and input templates

--- scripts/cromwell/copy_cromwell_results.sh
+++ scripts/cromwell/copy_cromwell_results.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-# USAGE: copy_cromwell_results.sh WORKFLOW_ID OUTPUT_FOLDER_NAME [DESTINATION_PATH]
+# USAGE: copy_cromwell_results.sh WORKFLOW_ID [DESTINATION_PATH] [SUB_FOLDER]
 # Copy all output files from cromwell workflow to flat folder at
 # destination path.
-# -If cromshell is not located at ${HOME}/Documents/cromshell/cromwell
+# -If cromshell is not available on the path as "cromshell"
 #  then define the ENV variable CROMSHELL with the appropriate path.
-# -If DESITNATION_PATH is not specified, it is assumed to be
+# -If DESTINATION_PATH is not specified, it is assumed to be
 #   "gs://broad-sv-dev-data/caller_benchmark/callers"
+# -if SUB_FOLDER is not defined, copy directly to DESTINATION_PATH
 set -Eeuo pipefail
 
 WORKFLOW_ID=$1
-CALLER_DIR=$2
 DESTINATION_PATH=${DESTINATION_PATH:-"gs://broad-sv-dev-data/caller_benchmark/callers"}
-DESTINATION_PATH=${3:-DESTINATION_PATH}
+DESTINATION_PATH=${2:-$DESTINATION_PATH}
+SUB_FOLDER=${3:-""}
 
-GCS_OUTPUT_DIR="${DESTINATION_PATH%/}/${CALLER_DIR}"
+if [ -n "$SUB_FOLDER" ]; then
+  GCS_OUTPUT_DIR="${DESTINATION_PATH%/}/${SUB_FOLDER%/}"
+else
+  GCS_OUTPUT_DIR="${DESTINATION_PATH%/}"
+fi
 
 # find the outputs
-CROMSHELL=${CROMSHELL:-${HOME}/Documents/cromshell/cromwell}
-METADATA=$($CROMSHELL -t 200 metadata $WORKFLOW_ID)
-OUTPUTS=$(echo "$METADATA" | jq '.outputs | .[]' | sed -e 's/ //g' -e 's/^"//' -e 's/,$//' -e 's/"$//' | grep -v '\]\|\[' | sort)
+CROMSHELL=${CROMSHELL:-cromshell}
+$CROMSHELL -t 200 metadata $WORKFLOW_ID > ./metadata.json
+OUTPUTS=$(jq '.outputs | .[]' ./metadata.json \
+          | sed -e 's/ //g' -e 's/^"//' -e 's/,$//' -e 's/"$//' \
+          | grep -v '\]\|\[' | grep -v "null" \
+          | sort)
 # copy the outputs to the requested GCS location
-echo "$OUTPUTS" | gsutil -m cp -I "$GCS_OUTPUT_DIR"
+printf "$OUTPUTS\n./metadata.json\n" | gsutil -m cp -I "$GCS_OUTPUT_DIR/"
