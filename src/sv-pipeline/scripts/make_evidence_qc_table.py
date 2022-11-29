574be9880060f72a123208780faf978697337ef1
Shadi Zaheri <74751641+shadizaheri@users.noreply.github.com>
2022-11-29 12:13:40 -0500

    Update make_evidence_qc_table.py

--- src/sv-pipeline/scripts/make_evidence_qc_table.py
+++ src/sv-pipeline/scripts/make_evidence_qc_table.py
@@ -62,7 +62,6 @@ def read_non_diploid(filename: str) -> pd.DataFrame:
     df_non_diploid = pd.read_csv(filename, sep="\t").T
     df_non_diploid = df_non_diploid.T.iloc[0:, 3:]
     nondiploid_counts = df_non_diploid[df_non_diploid < 0.05].count()
-
     nondiploid_counts_df = pd.DataFrame(nondiploid_counts, columns=["nondiploid_bins"]).rename_axis(
         ID_COL).reset_index()
     return nondiploid_counts_df
