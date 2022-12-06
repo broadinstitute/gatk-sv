d4c9cb1b51ebea0c92e647583e11ab9f36ec2b21
Mark Walker <markw@broadinstitute.org>
2022-12-06 12:46:02 -0500

    Fix annotate gq recal script

--- src/sv-pipeline/scripts/annotate_gq_recalibrated_vcf.py
+++ src/sv-pipeline/scripts/annotate_gq_recalibrated_vcf.py
@@ -16,7 +16,7 @@ _gt_non_ref_or_no_call_map = dict()
 def _is_non_ref_or_no_call(gt):
     s = _gt_non_ref_or_no_call_map.get(gt, None)
     if s is None:
-        s = any([a is not None or a > 0 for a in gt]) or all([a is None for a in gt])
+        s = any([a is not None and a > 0 for a in gt]) or all([a is None for a in gt])
         _gt_non_ref_or_no_call_map[gt] = s
     return s
 
@@ -36,8 +36,8 @@ def annotate_ncr(vcf, fout):
             continue
         gt_list = record.samples.values()
         n_no_call = sum([_is_no_call(gt['GT']) for gt in gt_list])
-        sl = [float(gt['SL']) for gt in gt_list if _gt_non_ref_or_no_call_map(gt['GT'])]
-        record.info['N_NO_CALL'] = n_no_call
+        sl = [float(gt['SL']) for gt in gt_list if _is_non_ref_or_no_call(gt['GT'])]
+        record.info['NCN'] = n_no_call
         record.info['NCR'] = n_no_call / n_samples
         record.info['SL_MEAN'] = sum(sl) / len(sl) if len(sl) > 0 else None
         record.info['SL_MAX'] = max(sl) if len(sl) > 0 else None
@@ -59,7 +59,7 @@ def main():
         vcf = pysam.VariantFile(args.vcf)
 
     header = vcf.header
-    header.add_line('##INFO=<ID=N_NO_CALL,Number=1,Type=Integer,Description="Number of no-call genotypes">')
+    header.add_line('##INFO=<ID=NCN,Number=1,Type=Integer,Description="Number of no-call genotypes">')
     header.add_line('##INFO=<ID=NCR,Number=1,Type=Float,Description="Rate of no-call genotypes">')
     header.add_line('##INFO=<ID=SL_MEAN,Number=1,Type=Float,Description="Mean SL of no-call and non-ref genotypes">')
     header.add_line('##INFO=<ID=SL_MIN,Number=1,Type=Float,Description="Min SL of no-call and non-ref genotypes">')
