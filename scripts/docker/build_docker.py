85d560583fc628f1372fb69d2ff0a9faa941cf7d
VJalili <VJalili@users.noreply.github.com>
2023-01-30 11:00:24 -0500

    A correct value for indent is the number of spaces.

--- scripts/docker/build_docker.py
+++ scripts/docker/build_docker.py
@@ -261,10 +261,10 @@ class ProjectBuilder:
             # update dockers.json with the new data
             if self.project_arguments.dry_run:
                 print(f"Write output dockers json at {output_json}")
-                print(json.dumps(new_dockers_json, indent="  "))
+                print(json.dumps(new_dockers_json, indent=2))
             else:
                 with open(output_json, "w") as f_out:
-                    json.dump(new_dockers_json, f_out, indent="  ")
+                    json.dump(new_dockers_json, f_out, indent=2)
 
     def get_build_priority(self, target_name: str) -> (int, str):
         """
