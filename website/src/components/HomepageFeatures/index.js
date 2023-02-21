57ff6e0064d08951bd170dd30051ad54664786e7
VJalili <VJalili@users.noreply.github.com>
2023-02-21 11:56:27 -0500

    Update text.

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -8,7 +8,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Built predominately to run in a cloud environment
+        Built and optimized for the Google Cloud Platform
       </>
     ),
   },
