cf482fb3467531cab1091392e0fd24bad48a5eb6
VJalili <VJalili@users.noreply.github.com>
2023-03-29 19:50:44 -0400

    Create empty output files when no input file is given.

--- src/str/combine_files_final.py
+++ src/str/combine_files_final.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import sys
 
-suffix = 'AllMetrices'
 file_name = sys.argv[1]
 
 files = []
 with open(file_name, "r") as fin:
     files = [line.rstrip() for line in fin]
-output_file = "REViewer_AllMetrices.txt"
+output_file = "reviewer_all_metrics.txt"
+missing_metrics_filename = "missing_metrics.txt"
 
 dfs = []
 for filename in files:
@@ -18,8 +18,12 @@ for filename in files:
     except pd.errors.EmptyDataError:
         print(f"Empty file: {filename}")
 
-df = pd.concat(dfs, axis=0, ignore_index=True)
-na_free = df.dropna()
-only_na = df[df.isna().any(axis=1)]
-na_free.to_csv(output_file,sep='\t',index=None)
-only_na.to_csv("missing_metrices.txt",sep='\t',index=None)
+if len(dfs) > 0:
+    df = pd.concat(dfs, axis=0, ignore_index=True)
+    na_free = df.dropna()
+    only_na = df[df.isna().any(axis=1)]
+    na_free.to_csv(output_file,sep='\t',index=None)
+    only_na.to_csv(missing_metrics_filename,sep='\t',index=None)
+else:
+    open(output_file, "w").close()
+    open(missing_metrics_filename, "w").close()
