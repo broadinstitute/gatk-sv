e745e1bafb82b7f7cb2d7ed70b86121c2895ac5a
VJalili <VJalili@users.noreply.github.com>
2023-03-29 11:26:00 -0400

    Add a script to merge all the results.

---
+++ src/str/combine_files_final.py
@@ -0,0 +1,15 @@
+import glob
+import pandas as pd
+import sys
+
+suffix = 'AllMetrices'
+file_name = sys.argv[1]
+
+output_file = file_name + "_REViewer_AllMetrices.txt"
+
+files = glob.glob('*' + suffix)
+df = pd.concat((pd.read_csv(f, sep='\t') for f in files))
+na_free = df.dropna()
+only_na = df[df.isna().any(axis=1)]
+na_free.to_csv(output_file,sep='\t',index=None)
+only_na.to_csv("missing_metrices.txt",sep='\t',index=None)
