107aeb12bd09ed837182d5379e2d9441bd28662b
VJalili <VJalili@users.noreply.github.com>
2023-03-29 15:16:45 -0400

    Modify the script to read the inputs from a file.

--- src/str/combine_files_final.py
+++ src/str/combine_files_final.py
@@ -5,9 +5,12 @@ import sys
 suffix = 'AllMetrices'
 file_name = sys.argv[1]
 
+files = []
+with open(file_name, "r") as fin:
+    files = fin.readlines()
 output_file = file_name + "_REViewer_AllMetrices.txt"
 
-files = glob.glob('*' + suffix)
+# files = glob.glob('*' + suffix)
 df = pd.concat((pd.read_csv(f, sep='\t') for f in files))
 na_free = df.dropna()
 only_na = df[df.isna().any(axis=1)]
