e70a0b1ae83d79401dfdc4a8e3b871846826646d
VJalili <VJalili@users.noreply.github.com>
2023-03-29 19:22:08 -0400

    update python script.

--- src/str/combine_files_final.py
+++ src/str/combine_files_final.py
@@ -1,4 +1,3 @@
-import glob
 import pandas as pd
 import sys
 
@@ -7,11 +6,19 @@ file_name = sys.argv[1]
 
 files = []
 with open(file_name, "r") as fin:
-    files = fin.readlines()
-output_file = file_name + "_REViewer_AllMetrices.txt"
+    files = [line.rstrip() for line in fin]
+output_file = "REViewer_AllMetrices.txt"
 
-# files = glob.glob('*' + suffix)
-df = pd.concat((pd.read_csv(f, sep='\t') for f in files))
+dfs = []
+for filename in files:
+    try:
+        df = pd.read_csv(filename, index_col=None, header=0, sep="\t")
+        if not df.empty:
+            dfs.append(df)
+    except pd.errors.EmptyDataError:
+        print(f"Empty file: {filename}")
+
+df = pd.concat(dfs, axis=0, ignore_index=True)
 na_free = df.dropna()
 only_na = df[df.isna().any(axis=1)]
 na_free.to_csv(output_file,sep='\t',index=None)
