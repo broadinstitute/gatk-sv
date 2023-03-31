5511f3846dfc4bc4a2b6f69c8a2c6b68049d44d6
VJalili <VJalili@users.noreply.github.com>
2023-03-31 15:49:01 -0400

    Fix a few bugs in the bash scripts.

--- src/str/count_nucleotides_from_reads_orange.sh
+++ src/str/count_nucleotides_from_reads_orange.sh
@@ -2,8 +2,8 @@
 image=$1
 file=$2
 
-seq 15 +10 20000 > RANGE_x
-seq 50 +15 20000 > RANGE
+seq 15 +10 20000 > RANGE_x_or
+seq 50 +15 20000 > RANGE_or
 
 StartX=$(cat $file | awk -v OFS="\t" '{if (NR >1) print $1}')
 EndX=$(cat $file | awk -v OFS="\t" '{if (NR >1) print $2}')
@@ -15,17 +15,17 @@ Limit=$(cat $file | awk -v OFS="\t" '{if (NR >1) print $NF}')
 #Extract the full list of reads
 #Y-axis upper haplotype
 
-cat $image  | awk '{ gsub("y=","",$3) }1' | awk '{print $3}' | tr -d '"' | awk -v y="$Limit" '{if ($1>31 && $1<y) print}' | awk '!seen[$0]++' > "$image"_TEMP
-cat "$image"_TEMP | while read line;do cat RANGE | grep -w $line; done > "$image"_TEMP2
-grep "\S" "$image"_TEMP2 |  awk '!seen[$0]++' > "$image"_TEMP3
+cat $image  | awk '{ gsub("y=","",$3) }1' | awk '{print $3}' | tr -d '"' | awk -v y="$Limit" '{if ($1>31 && $1<y) print}' | awk '!seen[$0]++' > "$image"_TEMP_or
+cat "$image"_TEMP_or | while read line;do cat RANGE_or | grep -w $line; done > "$image"_TEMP2_or
+grep "\S" "$image"_TEMP2_or |  awk '!seen[$0]++' > "$image"_TEMP3_or
 #cat RANGE_x | awk -v x="$StartX" -v xe="$EndX" '{if ($1>x && $1<xe) print}' > "$image"_TEMP_X
 
 
 #Y-axis bottom haplotype
 Limit_bottom=$((Limit+20))
-cat $image  | awk '{ gsub("y=","",$3) }1' | awk '{print $3}' | tr -d '"' | awk -v y="$Limit_bottom" '{if ($1>y) print}' | awk '!seen[$0]++' > "$image"_TEMP_bottom
-cat "$image"_TEMP_bottom | while read line;do cat RANGE | grep -w $line; done > "$image"_TEMP2_bottom
-grep "\S" "$image"_TEMP2_bottom |  awk '!seen[$0]++' > "$image"_TEMP3_bottom
+cat $image  | awk '{ gsub("y=","",$3) }1' | awk '{print $3}' | tr -d '"' | awk -v y="$Limit_bottom" '{if ($1>y) print}' | awk '!seen[$0]++' > "$image"_TEMP_bottom_or
+cat "$image"_TEMP_bottom_or | while read line;do cat RANGE_or | grep -w $line; done > "$image"_TEMP2_bottom_or
+grep "\S" "$image"_TEMP2_bottom_or |  awk '!seen[$0]++' > "$image"_TEMP3_bottom_or
 #cat RANGE_x | awk -v x="$StartX_bottom" -v xe="$EndX_bottom" '{if ($1>x && $1<xe) print}' > "$image"_TEMP_X_bottom
 
 Acount_orange=0
@@ -42,7 +42,7 @@ Ccount_orange_bottom=0
 #HAPLOID REGION
 if [ $Limit == 'NA' ] && [ $StartX != 'NA' ]
 then
-  Lines=$(cat "$image"_TEMP3)
+  Lines=$(cat "$image"_TEMP3_or)
   for line in $Lines
   do
     Yaxis=$(echo $line | awk '{print "y=""\""$1"\""}' | awk '{ gsub (" ", "", $0); print}')
@@ -58,13 +58,13 @@ then
     echo $sum_tot
     Total_orange=$((Acount_orange+Tcount_orange+Gcount_orange+Ccount_orange))
     Total_orange_bottom="NA"
-  done >> "$image"_Sum_Line.txt
+  done >> "$image"_Sum_Line_or.txt
   Total_orange=$((Acount_orange+Tcount_orange+Gcount_orange+Ccount_orange))
   Total_orange_bottom="NA" 
-  median=$(sort -n "$image"_Sum_Line.txt | awk ' { a[i++]=$1; } END { print a[int(i/2)]; }')
+  median=$(sort -n "$image"_Sum_Line_or.txt | awk ' { a[i++]=$1; } END { print a[int(i/2)]; }')
   median_bottom="NA"
-  Total_read=$(wc -l "$image"_Sum_Line.txt |awk '{print $1}')
-  Count_Orange_Upper_reads_with_interruptions=$(cat "$image"_Sum_Line.txt | awk '{if ($1 !=0) print $0}' | wc -l | awk '{print $1}')
+  Total_read=$(wc -l "$image"_Sum_Line_or.txt |awk '{print $1}')
+  Count_Orange_Upper_reads_with_interruptions=$(cat "$image"_Sum_Line_or.txt | awk '{if ($1 !=0) print $0}' | wc -l | awk '{print $1}')
   Total_read_bottom="NA"
   Total_orange_bottom="NA"
   Acount_orange_bottom="NA"
@@ -80,7 +80,7 @@ sum_tot=0
 #DIPLOID REGION
 if [ $Limit != 'NA' ] && [ $StartX != 'NA' ]
 then
-  Lines=$(cat "$image"_TEMP3)
+  Lines=$(cat "$image"_TEMP3_or)
   for line in $Lines
   do
     Yaxis=$(echo $line | awk '{print "y=""\""$1"\""}' | awk '{ gsub (" ", "", $0); print}')
@@ -94,17 +94,17 @@ then
     Ccount_orange=$((Ccount_orange+C))
     sum_tot=$((C+A+T+G))
     echo $sum_tot
-  done >> "$image"_Sum_Line.txt
+  done >> "$image"_Sum_Line_or.txt
   Total_orange=$((Acount_orange+Tcount_orange+Gcount_orange+Ccount_orange))
-  median=$(sort -n "$image"_Sum_Line.txt | awk ' { a[i++]=$1; } END { print a[int(i/2)]; }')
-  Total_read=$(wc -l "$image"_Sum_Line.txt |awk '{print $1}') 
-  Count_Orange_Upper_reads_with_interruptions=$(cat "$image"_Sum_Line.txt | awk '{if ($1 !=0) print $0}' | wc -l | awk '{print $1}')
+  median=$(sort -n "$image"_Sum_Line_or.txt | awk ' { a[i++]=$1; } END { print a[int(i/2)]; }')
+  Total_read=$(wc -l "$image"_Sum_Line_or.txt |awk '{print $1}') 
+  Count_Orange_Upper_reads_with_interruptions=$(cat "$image"_Sum_Line_or.txt | awk '{if ($1 !=0) print $0}' | wc -l | awk '{print $1}')
 fi
 sum_tot_bottom=0
 
 if [ $Limit != 'NA' ] && [ $StartX != 'NA' ]
 then  
-  Lines2=$(cat "$image"_TEMP3_bottom)
+  Lines2=$(cat "$image"_TEMP3_bottom_or)
   for line2 in $Lines2
   do
     Yaxis_bottom=$(echo $line2 | awk '{print "y=""\""$1"\""}' | awk '{ gsub (" ", "", $0); print}')    
@@ -118,11 +118,11 @@ then
     Ccount_orange_bottom=$((Ccount_orange_bottom+C_bottom))      
     sum_tot_bottom=$((C_bottom+A_bottom+T_bottom+G_bottom))
     echo $sum_tot_bottom
-  done >> "$image"_Sum_Line_bottom.txt
-  median_bottom=$(sort -n "$image"_Sum_Line_bottom.txt | awk ' { a[i++]=$1; } END { print a[int(i/2)]; }')
-  Total_read_bottom=$(wc -l "$image"_Sum_Line_bottom.txt |awk '{print $1}') 
+  done >> "$image"_Sum_Line_bottom_or.txt
+  median_bottom=$(sort -n "$image"_Sum_Line_bottom_or.txt | awk ' { a[i++]=$1; } END { print a[int(i/2)]; }')
+  Total_read_bottom=$(wc -l "$image"_Sum_Line_bottom_or.txt |awk '{print $1}') 
   Total_orange_bottom=$((Acount_orange_bottom+Tcount_orange_bottom+Gcount_orange_bottom+Ccount_orange_bottom))
-  Count_Orange_Bottom_reads_with_interruptions=$(cat "$image"_Sum_Line_bottom.txt | awk '{if ($1 !=0) print $0}' | wc -l | awk '{print $1}')
+  Count_Orange_Bottom_reads_with_interruptions=$(cat "$image"_Sum_Line_bottom_or.txt | awk '{if ($1 !=0) print $0}' | wc -l | awk '{print $1}')
 fi
 
 if [ $Limit == 'NA' ] && [ $StartX == 'NA' ]
@@ -144,15 +144,16 @@ then
 fi
 
 
-rm "$image"_TEMP
-rm "$image"_TEMP2
-rm "$image"_TEMP3
-rm "$image"_TEMP_bottom
-rm "$image"_TEMP2_bottom
-rm "$image"_TEMP3_bottom
-rm RANGE_x
-rm RANGE
-rm "$image"_Sum_Line_bottom.txt
-rm "$image"_Sum_Line.txt
+rm "$image"_TEMP_or
+rm "$image"_TEMP2_or
+rm "$image"_TEMP3_or
+rm "$image"_TEMP_bottom_or
+rm "$image"_TEMP2_bottom_or
+rm "$image"_TEMP3_bottom_or
+rm RANGE_x_or
+rm RANGE_or
+rm "$image"_Sum_Line_bottom_or.txt
+rm "$image"_Sum_Line_or.txt
 
-printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' A_upper_Orange T_upper_Orange G_upper_Orange C_upper_Orange Total_upper_Orange Median_orange_upper Total_read_upper Count_Orange_Upper_reads_with_interruptions A_bottom_Orange T_bottom_Orange G_bottom_Orange C_bottom_Orange Total_bottom_Orange Median_orange_bottom Total_read_bottom Count_Orange_Bottom_reads_with_interruptions $Acount_orange $Tcount_orange $Gcount_orange $Ccount_orange $Total_orange $median $Total_read $Count_Orange_Upper_reads_with_interruptions $Acount_orange_bottom $Tcount_orange_bottom $Gcount_orange_bottom $Ccount_orange_bottom $Total_orange_bottom $median_bottom $Total_read_bottom $Count_Orange_Bottom_reads_with_interruptions
+
+printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' A_upper_Orange T_upper_Orange G_upper_Orange C_upper_Orange Total_upper_Orange Median_orange_upper Total_read_upper Count_Orange_Upper_reads_with_interruptions A_bottom_Orange T_bottom_Orange G_bottom_Orange C_bottom_Orange Total_bottom_Orange Median_orange_bottom Total_read_bottom Count_Orange_Bottom_reads_with_interruptions $Acount_orange $Tcount_orange $Gcount_orange $Ccount_orange $Total_orange $median $Total_read $Count_Orange_Upper_reads_with_interruptions $Acount_orange_bottom $Tcount_orange_bottom $Gcount_orange_bottom $Ccount_orange_bottom $Total_orange_bottom $median_bottom $Total_read_bottom $Count_Orange_Bottom_reads_with_interruptions
