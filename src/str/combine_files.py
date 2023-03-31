bb6ba7933f1c8f2437f6cc968d83870883efb93a
VJalili <VJalili@users.noreply.github.com>
2023-03-31 18:00:25 -0400

    Remove the old scripts to combine and merge files. These scripts are replaced by other scripts.

--- src/str/combine_files.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-
-with open(sys.argv[1]) as f1, open(sys.argv[2]) as f2, open(sys.argv[3]) as f3, open(sys.argv[4]) as f4, open(sys.argv[5], "w") as f5:
-    for x, y, z, t in zip(f1, f2, f3,f4):
-          f5.write(x.strip() + "\t" + y.strip() + '\t' + z.strip() + '\t' + t.strip() + '\n')
