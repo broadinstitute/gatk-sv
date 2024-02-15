699f6399379108f22ff2bd54b820f21abb69aa0d
VJalili <VJalili@users.noreply.github.com>
2024-02-15 08:11:58 -0500

    Fix linting errors.

--- src/denovo/denovo_outliers.py
+++ src/denovo/denovo_outliers.py
@@ -16,7 +16,7 @@ bed = bed_original
 sample_counts = bed['sample'].value_counts()
 q3, q1 = np.percentile(sample_counts.tolist(), [75, 25])
 iqr = q3 - q1
-count_threshold = int(math.ceil((1.5*iqr + q3)*3))
+count_threshold = int(math.ceil((1.5 * iqr + q3) * 3))
 sample_counts2 = sample_counts.rename_axis('sample').reset_index(name='count')
 samples_keep = sample_counts2[(sample_counts2['count'] <= count_threshold)]['sample'].tolist()
 
@@ -24,7 +24,7 @@ samples_keep = sample_counts2[(sample_counts2['count'] <= count_threshold)]['sam
 output = bed[(bed['sample'].isin(samples_keep))]
 output_outliers = bed[(~bed['sample'].isin(samples_keep))]
 
-bed_original.loc[~(bed_original['sample'].isin(samples_keep)) & bed_original['is_de_novo'] == True, 'filter_flag'] = 'sample_outlier'
+bed_original.loc[~(bed_original['sample'].isin(samples_keep)) & bed_original['is_de_novo'], 'filter_flag'] = 'sample_outlier'
 
 # Write output
 output.to_csv(path_or_buf='final.denovo.merged.bed', mode='a', index=False, sep='\t', header=True)
