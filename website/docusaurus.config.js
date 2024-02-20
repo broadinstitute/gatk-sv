d09bd64ac85344cd5d024d6f466c9729709cfb12
VJalili <VJalili@users.noreply.github.com>
2024-02-20 17:17:30 -0500

    Add more languages for code blocks.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer').themes.github;
-const darkCodeTheme = require('prism-react-renderer').themes.dracula;
+import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -119,8 +118,11 @@ const config = {
         contextualSearch: true
     },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.vsDark,
+        additionalLanguages: [
+            'bash', 'powershell', 'json', 'json5', 'r', 'awk', 'jq', 'diff',
+            'git', 'ignore', 'log', 'makefile', 'regex', 'docker']
       },
       docs: {
         sidebar: {
