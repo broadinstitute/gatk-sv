b18605a42ad0f595f150dc9ce05044da60f46219
Mark Walker <markw@broadinstitute.org>
2024-09-16 11:55:02 -0400

    Filter legacy records with invalid coords (needs testing)

--- src/sv-pipeline/04_variant_resolution/scripts/reformat_genotyped_vcf.py
+++ src/sv-pipeline/04_variant_resolution/scripts/reformat_genotyped_vcf.py
@@ -3,10 +3,12 @@
 #
 
 """
-Replaces EV field numeric codes with strings and fixes alleles for multi-allelic CNVs
+Replaces EV field numeric codes with strings and fixes alleles for multi-allelic CNVs. Optionally drops records with
+invalid coordinates.
 """
 
 import argparse
+import logging
 import pysam
 import sys
 
@@ -49,7 +51,59 @@ def get_new_header(header):
     return new_header
 
 
-def process_record(record):
+def valid_coord(chrom, coord, ref_contig_length_dict):
+    if chrom not in ref_contig_length_dict:
+        raise ValueError(f"Contig not found in reference: {chrom}")
+    result = 1 <= coord <= ref_contig_length_dict[chrom]
+    if not result:
+        logging.warning(f"Invalid coordinate {chrom}:{coord}")
+    return result
+
+
+def get_startswith_match_in_list(substr, str_list):
+    matches = [t for t in str_list if t.startswith(substr)]
+    if len(matches) == 0:
+        return None
+    elif len(matches) == 1:
+        if matches[0] == ".":
+            return None
+        else:
+            return matches[0]
+    else:
+        raise ValueError(f"More than one match for {substr} in {str_list}")
+
+
+def valid_record_coordinates(record_tokens, ref_contig_length_dict):
+    chrom = record_tokens[0]
+    pos = int(record_tokens[1])
+    info_tokens = record_tokens[7].split(';')
+    end = get_startswith_match_in_list("CHR2=", info_tokens)
+    if end is not None:
+        end = int(end)
+    chr2 = get_startswith_match_in_list("CHR2=", info_tokens)
+    end2 = get_startswith_match_in_list("END2=", info_tokens)
+    if end2 is not None:
+        end2 = int(end2)
+    if not valid_coord(chrom=chrom,
+                       coord=pos,
+                       ref_contig_length_dict=ref_contig_length_dict):
+        return False
+    elif not valid_coord(chrom=chrom,
+                         coord=end,
+                         ref_contig_length_dict=ref_contig_length_dict):
+        return False
+    elif end2 is not None:
+        # For END2 check: use CHR2 if available, otherwise default to CHROM
+        if chr2 is None:
+            chr2 = chrom
+        if not valid_coord(chrom=chr2,
+                           coord=end2,
+                           ref_contig_length_dict=ref_contig_length_dict):
+            return False
+    return True
+
+
+def process_record(record, drop_invalid_coords, ref_contig_length_dict):
     # Fix multi-allelic CNV alts (legacy)
     if record.alts[0].startswith('<CN'):
         record.alts = ('<CNV>',)
@@ -59,6 +113,9 @@ def process_record(record):
     # Since pysam messes with some of the formatting (i.e. END limitation) we parse the string and replace
     max_ev = len(EVIDENCE_LIST) - 1
     record_tokens = str(record).strip().split('\t')
+    if drop_invalid_coords:
+        if not valid_record_coordinates(record_tokens=record_tokens, ref_contig_length_dict=ref_contig_length_dict):
+            return None
     format_keys = record_tokens[8].split(':')
     gt_index = format_keys.index('GT')
     ev_index = format_keys.index('EV')
@@ -83,19 +140,43 @@ def process_record(record):
     return '\t'.join(new_record_tokens)
 
 
+def parse_reference_fai(path):
+    if not path:
+        raise ValueError("Reference index required if using --drop-invalid-coords")
+    with open(path) as f:
+        result = dict()
+        for line in f:
+            tokens = line.strip().split('\t')
+            chrom = tokens[0]
+            length = int(tokens[1])
+            result[chrom] = length
+        return result
+
+
 def main():
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('vcf')
+    parser.add_argument("--drop-invalid-coords", action='store_true',
+                        help="Drop records with invalid coordinates, i.e. POS/END/END2 greater than chromosome length")
+    parser.add_argument("--reference-fai", type=str, required=False,
+                        help="Reference fasta index (.fai), required only if using --drop-invalid-coords")
     args = parser.parse_args()
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+    if args.drop_invalid_coords:
+        ref_contig_length_dict = parse_reference_fai(args.reference_fai)
 
     with pysam.VariantFile(args.vcf) as vcf:
         new_header = get_new_header(vcf.header)
         sys.stdout.write(str(new_header))
         for record in vcf:
-            new_record = process_record(record)
-            sys.stdout.write(new_record + "\n")
+            new_record = process_record(record=record,
+                                        drop_invalid_coords=args.drop_invalid_coords,
+                                        ref_contig_length_dict=ref_contig_length_dict)
+            if new_record is not None:
+                sys.stdout.write(new_record + "\n")
 
 
 if __name__ == '__main__':
