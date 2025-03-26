32c040a7dd15f22520c95c7268d846ca258ef9ac
VJalili <VJalili@users.noreply.github.com>
2025-03-26 10:18:46 -0400

    implement scramble part 2

--- src/bash_workflows/scramble.sh
+++ src/bash_workflows/scramble.sh
@@ -49,3 +49,33 @@ while read region; do
   time /app/scramble-gatk-sv/cluster_identifier/src/build/cluster_identifier -l -s ${MIN_CLIPPED_READS} -r "${region}" -t "${reference_fasta}" "${bam_or_cram_file}" \
     | gzip >> "${sample_name}".scramble_clusters.tsv.gz
 done < "${regions_list}"
+
+
+clusters_file="${sample_name}.scramble_clusters.tsv.gz"
+
+# In case it is re-run, the script will wait for a response
+# on override the existing file, that may not work in a pipeline.
+rm test.scramble.tsv.gz
+
+# ScramblePart2
+# -------------
+
+xDir=$PWD
+clusterFile=$xDir/clusters
+scrambleDir="/app/scramble-gatk-sv"
+meiRef=$scrambleDir/cluster_analysis/resources/MEI_consensus_seqs.fa
+
+# create a blast db from the reference
+cat "${reference_fasta}" | makeblastdb -in - -parse_seqids -title ref -dbtype nucl -out ref
+
+gunzip -c "${clusters_file}" > $clusterFile
+
+# Produce ${clusterFile}_MEIs.txt
+Rscript --vanilla $scrambleDir/cluster_analysis/bin/SCRAMble.R --out-name $clusterFile \
+        --cluster-file $clusterFile --install-dir $scrambleDir/cluster_analysis/bin \
+        --mei-refs $meiRef --ref $xDir/ref --no-vcf --eval-meis --cores "${part2_threads}" \
+        --pct-align "${percent_align_cutoff}" -n $MIN_CLIPPED_READS --mei-score "${alignment_score_cutoff}"
+
+# Save raw outputs
+mv ${clusterFile}_MEIs.txt "${sample_name}".scramble.tsv
+gzip "${sample_name}".scramble.tsv
