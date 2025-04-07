fd7cad8891f429d9adec0567e88b82794b4b9258
VJalili <VJalili@users.noreply.github.com>
2025-04-07 13:05:23 -0400

    add collect_sv_evidence.sh

---
+++ src/bash_workflows/collect_sv_evidence.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+bam_or_cram_file=$1
+bam_or_cram_index=$2
+sample_id=$3
+reference_fasta=$4
+reference_index=$5
+reference_dict=$6
+sd_locs_vcf=$7
+preprocessed_intervals=$8
+site_depth_min_mapq=${9:-6}
+site_depth_min_baseq=${10:-10}
+primary_contigs_list="${11:-}"
+gatk_jar_override="${12:-/root/gatk.jar}"
+command_mem_mb=${13:-3250}
+
+export GATK_LOCAL_JAR="$gatk_jar_override"
+
+primary_contigs_arg=""
+if [[ -n "$primary_contigs_list" ]]; then
+  primary_contigs_arg="-L $primary_contigs_list"
+fi
+
+/gatk/gatk --java-options "-Xmx${command_mem_mb}m" CollectSVEvidence \
+    -I "${bam_or_cram_file}" \
+    --sample-name "${sample_id}" \
+    -F "${sd_locs_vcf}" \
+    -SR "${sample_id}.sr.txt.gz" \
+    -PE "${sample_id}.pe.txt.gz" \
+    -SD "${sample_id}.sd.txt.gz" \
+    --site-depth-min-mapq "${site_depth_min_mapq}" \
+    --site-depth-min-baseq "${site_depth_min_baseq}" \
+    -R "${reference_fasta}" \
+    ${primary_contigs_arg} \
+    --read-filter NonZeroReferenceLengthAlignmentReadFilter
