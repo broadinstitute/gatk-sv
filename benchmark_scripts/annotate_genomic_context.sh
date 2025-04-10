0299943f3b17fc9b36e48fb42c4eb6233a22ea3b
xuefzhao <xuefzhao@umich.edu>
2025-04-10 12:27:14 -0400

    script to annotate genomic context

--- benchmark_scripts/annotate_genomic_context.sh
+++ benchmark_scripts/annotate_genomic_context.sh
@@ -8,32 +8,45 @@
 
 set -e
 
-###USAGE
-usage(){
+########################################
+# USAGE
+########################################
+usage() {
 cat <<EOF
 
-usage: compare_callsets.sh [options] INPUT OUTPUT RepMask SegDup SimpRep
+Usage: compare_callsets.sh [options] INPUT OUTPUT RepMask SegDup SimpRep
 
-Helper tool to annotate genomic context of SVs
+Helper tool to annotate genomic context of SVs.
 
 Positional arguments:
-  INPUT            SV callset to be benchmarked
-  OUTPUT            Ground truth SV callset to use for benchmarking
-  RepMask     repeat masked regions in bed format
-  SegDup      segmental duplicated regions in bed format
-  SimpRep     simple repeated regions in bed format
-
+  INPUT       SV callset to be benchmarked (BED format)
+  OUTPUT      Path for output annotated callset
+  RepMask     RepeatMasker regions (BED format)
+  SegDup      Segmental duplications (BED format)
+  SimpRep     Simple repeats (BED format)
 
 Notes:
-  1) INPUT are expected to be BED3+ formatted files
-  2) SET1 must have at least the following five columns, in order:
-     chr, start, end, SV ID, SV type, SV length
- 
+  - INPUT must be a BED3+ file with at least the following columns:
+    [chr, start, end, SV ID, SV type, SV length]
+  - OUTPUT is a filename for the annotated result.
+  - Requires bedtools and Rscript with annotate_genomic_context_helper.R.
+
+Options:
+  -h, --help  Show this help message and exit
+
+Example:
+  ./compare_callsets.sh input.bed.gz annotated_output.bed.gz repmask.bed segdup.bed simple_repeats.bed
+
 EOF
 }
 
-
-###PARSE ARGS
+########################################
+# PARSE ARGS
+########################################
+if [[ "$1" == "-h" || "$1" == "--help" || "$#" -ne 5 ]]; then
+  usage
+  exit 1
+fi
 
 INPUT=$1
 OUTPUT=$2
@@ -41,27 +54,32 @@ RepMask=$3
 SegDup=$4
 SimpRep=$5
 
-###SET BIN
+########################################
+# SETUP
+########################################
 BIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+OVRTMP=$(mktemp -d)
 
+########################################
+# PROCESSING
+########################################
+zcat "${INPUT}" | cut -f1-6  > "${OVRTMP}/tmp.sites"
 
-mkdir US_RM_SD_SR/
-zcat ${INPUT} | cut -f1-6  > US_RM_SD_SR/tmp.sites
-awk '{print $1,$2,$2,$4,$5,$6}'  US_RM_SD_SR/tmp.sites  | grep -v "#" | sed -e 's/ /\t/g' > US_RM_SD_SR/il_inte.le_bp
-awk '{print $1,$3,$3,$4,$5,$6}'  US_RM_SD_SR/tmp.sites  | grep -v "#" | sed -e 's/ /\t/g' > US_RM_SD_SR/il_inte.ri_bp
-zcat ${INPUT} | awk '{if ($5=="DEL" || $5=="DUP" || $5=="CNV" ) print}' | awk '{if ($3-$2>5000) print}' | cut -f1-5 >  US_RM_SD_SR/il_inte.lg_cnv
+awk '{print $1,$2,$2,$4,$5,$6}' "${OVRTMP}/tmp.sites" | grep -v "#" | sed -e 's/ /\t/g' > "${OVRTMP}/il_inte.le_bp"
+awk '{print $1,$3,$3,$4,$5,$6}' "${OVRTMP}/tmp.sites" | grep -v "#" | sed -e 's/ /\t/g' > "${OVRTMP}/il_inte.ri_bp"
 
-bedtools coverage -a US_RM_SD_SR/il_inte.le_bp -b ${SimpRep}  | awk '{if ($NF>0) print}'> US_RM_SD_SR/il_inte.le_bp.vs.SR
-bedtools coverage -a US_RM_SD_SR/il_inte.le_bp -b ${SegDup}   | awk '{if ($NF>0) print}'> US_RM_SD_SR/il_inte.le_bp.vs.SD
-bedtools coverage -a US_RM_SD_SR/il_inte.le_bp -b ${RepMask}  | awk '{if ($NF>0) print}'> US_RM_SD_SR/il_inte.le_bp.vs.RM
+zcat "${INPUT}" | awk '{if ($5=="DEL" || $5=="DUP" || $5=="CNV" ) print}' | awk '{if ($3-$2>5000) print}' | cut -f1-5 > "${OVRTMP}/il_inte.lg_cnv"
 
-bedtools coverage -a US_RM_SD_SR/il_inte.ri_bp -b ${SimpRep}  | awk '{if ($NF>0) print}'> US_RM_SD_SR/il_inte.ri_bp.vs.SR
-bedtools coverage -a US_RM_SD_SR/il_inte.ri_bp -b ${SegDup}   | awk '{if ($NF>0) print}'> US_RM_SD_SR/il_inte.ri_bp.vs.SD
-bedtools coverage -a US_RM_SD_SR/il_inte.ri_bp -b ${RepMask}  | awk '{if ($NF>0) print}'> US_RM_SD_SR/il_inte.ri_bp.vs.RM
+bedtools coverage -a "${OVRTMP}/il_inte.le_bp" -b "${SimpRep}" | awk '{if ($NF>0) print}' > "${OVRTMP}/il_inte.le_bp.vs.SR"
+bedtools coverage -a "${OVRTMP}/il_inte.le_bp" -b "${SegDup}"  | awk '{if ($NF>0) print}' > "${OVRTMP}/il_inte.le_bp.vs.SD"
+bedtools coverage -a "${OVRTMP}/il_inte.le_bp" -b "${RepMask}" | awk '{if ($NF>0) print}' > "${OVRTMP}/il_inte.le_bp.vs.RM"
 
-bedtools coverage -a US_RM_SD_SR/il_inte.lg_cnv -b ${SimpRep}  > US_RM_SD_SR/il_inte.lg_cnv.vs.SR
-bedtools coverage -a US_RM_SD_SR/il_inte.lg_cnv -b ${SegDup}   > US_RM_SD_SR/il_inte.lg_cnv.vs.SD
-bedtools coverage -a US_RM_SD_SR/il_inte.lg_cnv -b ${RepMask}  > US_RM_SD_SR/il_inte.lg_cnv.vs.RM
+bedtools coverage -a "${OVRTMP}/il_inte.ri_bp" -b "${SimpRep}" | awk '{if ($NF>0) print}' > "${OVRTMP}/il_inte.ri_bp.vs.SR"
+bedtools coverage -a "${OVRTMP}/il_inte.ri_bp" -b "${SegDup}"  | awk '{if ($NF>0) print}' > "${OVRTMP}/il_inte.ri_bp.vs.SD"
+bedtools coverage -a "${OVRTMP}/il_inte.ri_bp" -b "${RepMask}" | awk '{if ($NF>0) print}' > "${OVRTMP}/il_inte.ri_bp.vs.RM"
 
-Rscript ${BIN}/annotate_genomic_context_helper.R -i ${INPUT} -o ${OUTPUT} -p "US_RM_SD_SR"
+bedtools coverage -a "${OVRTMP}/il_inte.lg_cnv" -b "${SimpRep}" > "${OVRTMP}/il_inte.lg_cnv.vs.SR"
+bedtools coverage -a "${OVRTMP}/il_inte.lg_cnv" -b "${SegDup}"  > "${OVRTMP}/il_inte.lg_cnv.vs.SD"
+bedtools coverage -a "${OVRTMP}/il_inte.lg_cnv" -b "${RepMask}" > "${OVRTMP}/il_inte.lg_cnv.vs.RM"
 
+Rscript "${BIN}/annotate_genomic_context_helper.R" -i "${INPUT}" -o "${OUTPUT}" -p "${OVRTMP}"
