9942cbc140e7679b93dd19a11a28436a43289222
VJalili <VJalili@users.noreply.github.com>
2025-04-11 10:09:04 -0400

    update whamg

--- src/bash_workflows/run_whamg.sh
+++ src/bash_workflows/run_whamg.sh
@@ -11,6 +11,8 @@ include_bed_file=$6
 primary_contigs_list=$7
 cpu_cores=${8:-4}
 
+include_bed_file_abs_path=$(realpath $include_bed_file)
+reference_fasta_abs_path=$(realpath $reference_fasta)
 
 chr_list=$(paste -s -d ',' "${primary_contigs_list}")
 
@@ -23,33 +25,38 @@ export GCS_OAUTH_TOKEN=`gcloud auth application-default print-access-token`
 
 echo "Converting cram to bam ..."
 # covert cram to bam
-samtools view -b1@ ${cpu_cores} -T "${reference_fasta}" "${cram_file}" > sample.bam
+#samtools view -b1@ ${cpu_cores} -T "${reference_fasta}" "${cram_file}" > sample.bam
 echo "Finished converting cram to bam."
 
 # index bam file
-samtools index -@ ${cpu_cores} sample.bam
+echo "Indexing the bam file ..."
+#samtools index -@ ${cpu_cores} sample.bam
+echo "Finished indexing the bam file."
 
 # ensure that index files are present in appropriate locations
-ln -s sample.bam.bai sample.bai
-if [ ! -e "${reference_fasta}.fai" ]; then
-  ln -s "${reference_index}" "${reference_fasta}.fai"
-fi
+#ln -s sample.bam.bai sample.bai
+#if [ ! -e "${reference_fasta}.fai" ]; then
+#  ln -s "${reference_index}" "${reference_fasta}.fai"
+#fi
 
 # run whamg on all specified intervals
-mkdir tmpVcfs
+echo "Running whamg on specified intervals ..."
+mkdir -p tmpVcfs
 cd tmpVcfs
-awk 'BEGIN{FS=OFS="\t"}{printf("%07d\t%s\n",NR,$1":"$2"-"$3)}' ${include_bed_file} |\
+
+awk 'BEGIN{FS=OFS="\t"}{printf("%07d\t%s\n",NR,$1":"$2"-"$3)}' "${include_bed_file_abs_path}" |\
   while read -r line interval; do
     vcfFile="$line.wham.vcf.gz"
     whamg \
-        -c ${chr_list}" \
+        -c "${chr_list}" \
         -x ${cpu_cores} \
-        -a "${reference_fasta}" \
+        -a "${reference_fasta_abs_path}" \
         -f ../sample.bam \
         -r $interval \
       | bgzip -c > $vcfFile
     bcftools index -t $vcfFile
   done
+echo "Finished running whamg on specified intervals."
 
 # We need to update both the VCF sample ID and the TAGS INFO field in the WHAM output VCFs.
 # WHAM uses both to store the sample identifier, and by default uses the SM identifier from the BAM file.
