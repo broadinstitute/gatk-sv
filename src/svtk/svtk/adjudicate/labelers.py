be277bb9f17c240372c828ee669fa180dd0e4d41
Mark Walker <markw@broadinstitute.org>
2025-08-07 14:01:17 -0400

    PEQ threshold 13

--- src/svtk/svtk/adjudicate/labelers.py
+++ src/svtk/svtk/adjudicate/labelers.py
@@ -34,7 +34,7 @@ class SR1TrainingLabeler(TrainingLabeler):
     def label_row(self, row):
         if (row.RD_Median_Separation < 0.15 and
                 row.BAF1_prob < 0.4 and
-                row.PEQ < 5):
+                row.PEQ < 13):
             return 'Fail'
         elif row.RD_Median_Separation >= 0.4 and row.BAF1_prob >= 0.9:
             return 'Pass'
