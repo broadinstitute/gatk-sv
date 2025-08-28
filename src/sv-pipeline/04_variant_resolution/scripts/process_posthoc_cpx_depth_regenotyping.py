a0efb7d911c2e5598495a8c686de49b6abe933ee
Karan Jaisingh <kjaising@broadinstitute.org>
2025-08-28 14:32:50 -0400

    Reversed string name in wrong pair of variants - resolved now (finally)

--- src/sv-pipeline/04_variant_resolution/scripts/process_posthoc_cpx_depth_regenotyping.py
+++ src/sv-pipeline/04_variant_resolution/scripts/process_posthoc_cpx_depth_regenotyping.py
@@ -645,8 +645,8 @@ def final_assessment(cleaned_genotype_counts, variants_to_reclassify, min_ddup_t
                     reason="INVERTED_DISPERSED_DUPLICATION_WITH_DELETION",
                     new_sv_type="CPX",
                     new_cpx_type="dDUP_iDEL",
-                    new_cpx_intervals=f"INV_{interval_string(dup_chrom, dup_start, dup_end)},"
-                                      f"DUP_{interval_string(dup_chrom, dup_start, dup_end)},"
+                    new_cpx_intervals=f"DUP_{interval_string(dup_chrom, dup_start, dup_end)},"
+                                      f"INV_{interval_string(dup_chrom, dup_start, dup_end)},"
                                       f"DEL_{r.sink_string()}",
                     new_svlen=dup_size + r.sink_size(),
                     new_source=f"DUP_{interval_string(dup_chrom, dup_start, dup_end)}",
@@ -661,8 +661,8 @@ def final_assessment(cleaned_genotype_counts, variants_to_reclassify, min_ddup_t
                     reason="INVERTED_DISPERSED_DUPLICATION",
                     new_sv_type="CPX",
                     new_cpx_type="dDUP",
-                    new_cpx_intervals=f"INV_{interval_string(dup_chrom, dup_start, dup_end)},"
-                                      f"DUP_{interval_string(dup_chrom, dup_start, dup_end)}",
+                    new_cpx_intervals=f"DUP_{interval_string(dup_chrom, dup_start, dup_end)},"
+                                      f"INV_{interval_string(dup_chrom, dup_start, dup_end)}",
 
                     new_svlen=dup_size,
                     new_source=f"DUP_{interval_string(dup_chrom, dup_start, dup_end)}",
