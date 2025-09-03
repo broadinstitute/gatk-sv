67c8b1e7ca47837a051200ba69731b1670ad8bc9
Karan Jaisingh <kjaising@broadinstitute.org>
2025-09-03 15:30:08 -0400

    Pin the version of pysam in python packages too

--- src/svtk/setup.py
+++ src/svtk/setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=[
         'numpy',
         'scipy',
-        'pysam>=0.11.2.2',
+        'pysam>=0.23.3',
         'pybedtools',
         'cython',
         'natsort',
