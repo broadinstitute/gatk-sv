baa308f654f5d587759e08943dd663429a4975bc
Vahid <VJalili@users.noreply.github.com>
2025-10-03 10:15:40 -0400

    check in cnmops.

--- src/sv_shell/cnmops.sh
+++ src/sv_shell/cnmops.sh
@@ -20,20 +20,15 @@ echo "JVM memory: $JVM_MAX_MEM"
 
 
 function CNSampleNormal() {
-  # TODO: check if these variables are correctly set
   local _chr=$1
   local _mode=$2
   local _r=$3
-  local _wd=$4
 
   echo "----------- Starting CN Sample Normal -------------"
   echo "chr: ${_chr}"
   echo "mode: ${_mode}"
   echo "r: ${_r}"
-  echo "Working dir: ${_wd}"
   echo "---------------------------------------------------"
-
-  cd "${_wd}"
 
   java "-Xmx${JVM_MAX_MEM}" -jar /opt/gatk.jar PrintSVEvidence \
     --sequence-dictionary "${ref_dict}" \
@@ -88,7 +83,41 @@ function CleanCNMops() {
   echo "./cnmops.gff1" > GFF.list
   /opt/WGD/bin/cleancnMOPS.sh -z -o calls/ -S "${exclude_list}" sample.list GFF.list
 
+  zcat calls/*/*.cnMOPS.DEL.bed.gz > DELS.bed
+  awk -v batch="${batch}_DEL_" 'BEGIN{OFS="\t"} {print $1,$2,$3,batch,$4,"cnmops"}' DELS.bed | cat -n |\
+  awk 'BEGIN{OFS="\t"} {print $2,$3,$4,$5$1,$6,"DEL",$7}' | sort -k1,1V -k2,2n > "${batch}.DEL.bed"
 
+  cat <(echo -e "#chr\tstart\tend\tname\tsample\tsvtype\tsources") "${batch}.DEL.bed"  > "${batch}.DEL.${prefix}.bed"
+
+  zcat calls/*/*.cnMOPS.DUP.bed.gz > DUPS.bed
+  awk -v batch="${batch}_DUP_" 'BEGIN{OFS="\t"} {print $1,$2,$3,batch,$4,"cnmops"}' DUPS.bed | cat -n |\
+  awk 'BEGIN{OFS="\t"} {print $2,$3,$4,$5$1,$6,"DUP",$7}' | sort -k1,1V -k2,2n > "${batch}.DUP.bed"
+
+  cat <(echo -e "#chr\tstart\tend\tname\tsample\tsvtype\tsources") "${batch}.DUP.bed"  > "${batch}.DUP.${prefix}.bed"
+
+  if [[ "${stitch_and_clean_large_events}" == "true" ]]; then
+    echo "stitching and cleaning large events."
+    mv "${batch}.DUP.${prefix}.bed" "${batch}.DUP.${prefix}.prestitch.bed"
+    mv "${batch}.DEL.${prefix}.bed" "${batch}.DEL.${prefix}.prestitch.bed"
+    cat "${chrom_file}" "${allo_file}" > contig.fai
+    svtk rdtest2vcf --contigs contig.fai "${batch}.DUP.${prefix}.prestitch.bed" sample.list dup.vcf.gz
+    svtk rdtest2vcf --contigs contig.fai "${batch}.DEL.${prefix}.prestitch.bed" sample.list del.vcf.gz
+    bash /opt/sv-pipeline/04_variant_resolution/scripts/stitch_fragmented_CNVs.sh -d dup.vcf.gz dup1.vcf.gz
+    bash /opt/sv-pipeline/04_variant_resolution/scripts/stitch_fragmented_CNVs.sh -d del.vcf.gz del1.vcf.gz
+    svtk vcf2bed dup1.vcf.gz dup1.bed
+    cat <(echo -e "#chr\tstart\tend\tname\tsample\tsvtype\tsources") \
+        <(awk -v OFS="\t" -v minsize="${min_size}" '{if($3-$2>minsize)print $1,$2,$3,$4,$6,$5,"cnmops_large"}' dup1.bed) > "${batch}.DUP.${prefix}.bed"
+    svtk vcf2bed del1.vcf.gz del1.bed
+    cat <(echo -e "#chr\tstart\tend\tname\tsample\tsvtype\tsources") \
+        <(awk -v OFS="\t" -v minsize="${min_size}" '{if($3-$2>minsize)print $1,$2,$3,$4,$6,$5,"cnmops_large"}' del1.bed) > "${batch}.DEL.${prefix}.bed"
+    echo "Finished stitching."
+  fi
+  bgzip -f "${batch}.DEL.${prefix}.bed"
+  tabix -f "${batch}.DEL.${prefix}.bed.gz"
+
+  bgzip -f "${batch}.DUP.${prefix}.bed"
+  tabix -f "${batch}.DUP.${prefix}.bed.gz"
+}
 
 
 # -------------------------------------------------------
@@ -118,8 +147,9 @@ fi
 working_dir=$(mktemp -d /wd_batch_evidence_merging_XXXXXXXX)
 working_dir="$(realpath ${working_dir})"
 cd "${working_dir}"
+echo "cnMOPS Working directory: ${working_dir}"
 
-
+batch=($(jq -r '.batch' "$input_json"))
 allo_file=($(jq -r '.allo_file' "$input_json"))
 chrom_file=($(jq -r '.chrom_file' "$input_json"))
 exclude_list=($(jq -r '.exclude_list' "$input_json"))
@@ -128,6 +158,9 @@ r1=($(jq -r '.r1' "$input_json"))
 r2=($(jq -r '.r2' "$input_json"))
 ref_dict=($(jq -r '.ref_dict' "$input_json"))
 bincov_matrix=($(jq -r '.bincov_matrix' "$input_json"))
+stitch_and_clean_large_events=($(jq -r '.stitch_and_clean_large_events' "$input_json"))
+min_size=($(jq -r '.min_size' "$input_json"))
+prefix=($(jq -r '.prefix' "$input_json"))
 
 
 # -------------------------------------------------------
@@ -135,42 +168,130 @@ bincov_matrix=($(jq -r '.bincov_matrix' "$input_json"))
 # -------------------------------------------------------
 
 
+male_r1_gff=()
+male_r2_gff=()
 allos=($(awk '{print $1}' "${allo_file}"))
-
 for allo in "${allos[@]}"; do
   # Male R2
   working_dir=$(mktemp -d /wd_cn_sample_normal_${allo}_${r2}_XXXXXXXX)
   working_dir="$(realpath ${working_dir})"
-  CNSampleNormal "${allo}" "1" "${r2}" "${working_dir}"
+  cd "${working_dir}"
+  CNSampleNormal "${allo}" "1" "${r2}"
+  male_r2_gff+=("${working_dir}/calls/cnMOPS.cnMOPS.gff")
 
   # Male R1
   working_dir=$(mktemp -d /wd_cn_sample_normal_${allo}_${r1}_XXXXXXXX)
   working_dir="$(realpath ${working_dir})"
-  CNSampleNormal "${allo}" "1" "${r1}" "${working_dir}"
+  cd "${working_dir}"
+  CNSampleNormal "${allo}" "1" "${r1}"
+  male_r1_gff+=("${working_dir}/calls/cnMOPS.cnMOPS.gff")
 done
 
+normal_r1_gff=()
+normal_r2_gff=()
 chroms=($(awk '{print $1}' "${chrom_file}"))
 for chrom in "${chroms[@]}"; do
   # Normal R2
   working_dir=$(mktemp -d /wd_cn_sample_normal_${chrom}_${r2}_XXXXXXXX)
   working_dir="$(realpath ${working_dir})"
-  CNSampleNormal "${chrom}" "normal" "${r2}" "${working_dir}"
+  cd "${working_dir}"
+  CNSampleNormal "${chrom}" "normal" "${r2}"
+  normal_r2_gff+=("${working_dir}/calls/cnMOPS.cnMOPS.gff")
 
   # Normal R1
   working_dir=$(mktemp -d /wd_cn_sample_normal_${chrom}_${r1}_XXXXXXXX)
   working_dir="$(realpath ${working_dir})"
-  CNSampleNormal "${chrom}" "normal" "${r1}" "${working_dir}"
+  cd "${working_dir}"
+  CNSampleNormal "${chrom}" "normal" "${r1}"
+  normal_r1_gff+=("${working_dir}/calls/cnMOPS.cnMOPS.gff")
 done
 
 
+# Female R2
+working_dir=$(mktemp -d /wd_cn_sample_normal_chrX_${r2}_XXXXXXXX)
+working_dir="$(realpath ${working_dir})"
+cd "${working_dir}"
+CNSampleNormal "chrX" "2" "${r2}"
+female_r2_gff=("${working_dir}/calls/cnMOPS.cnMOPS.gff")
 
-first_row_string="${Allos[0]}"
+# Female R1
+working_dir=$(mktemp -d /wd_cn_sample_normal_chrX_${r1}_XXXXXXXX)
+working_dir="$(realpath ${working_dir})"
+cd "${working_dir}"
+CNSampleNormal "chrX" "2" "${r1}"
+female_r1_gff=("${working_dir}/calls/cnMOPS.cnMOPS.gff")
 
-echo "${first_row_string}"
+
+# TODO: TEMP TEST
+#mapfile -t normal_r1_gff < <(find /inputs_sv_shell/ -name "NR1_*_cnMOPS.cnMOPS.gff" | sort -V)
+#mapfile -t normal_r2_gff < <(find /inputs_sv_shell/ -name "NR2_*_cnMOPS.cnMOPS.gff" | sort -V)
+#mapfile -t male_r1_gff < <(find /inputs_sv_shell/ -name "MR1_*_cnMOPS.cnMOPS.gff" | sort -V)
+#mapfile -t male_r2_gff < <(find /inputs_sv_shell/ -name "MR2_*_cnMOPS.cnMOPS.gff" | sort -V)
+#mapfile -t female_r1_gff < <(find /inputs_sv_shell/ -name "FR1_*_cnMOPS.cnMOPS.gff" | sort -V)
+#mapfile -t female_r2_gff < <(find /inputs_sv_shell/ -name "FR2_*_cnMOPS.cnMOPS.gff" | sort -V)
+#sample_list="/inputs_sv_shell/combined_ped_file.ped"
+#exclude_list="/inputs_sv_shell/GRCh38_Nmask.bed"
+
+
+
+# CleanCNMops
+working_dir=$(mktemp -d /wd_clean_cnmops_XXXXXXXX)
+working_dir="$(realpath ${working_dir})"
+cd "${working_dir}"
+
+#echo "--------------- ${working_dir}"
+#echo "Bash script is using these files:"
+#echo "${normal_r1_gff[@]}" "${normal_r2_gff[@]}" "${male_r1_gff[@]}" "${male_r2_gff[@]}" "${female_r1_gff[@]}" "${female_r2_gff[@]}"
+#echo "********************************************"
+cnmops_gff_filename="$(realpath ${working_dir}/cnmops.gff)"
+cat \
+  "${normal_r1_gff[@]}" \
+  "${normal_r2_gff[@]}" \
+  "${male_r1_gff[@]}" \
+  "${male_r2_gff[@]}" \
+  "${female_r1_gff[@]}" \
+  "${female_r2_gff[@]}" > "${cnmops_gff_filename}"
+
+CleanCNMops "${ped_file}" "${cnmops_gff_filename}"
+
+echo "Finished running CleanCNMops."
+#first_row_string="${Allos[0]}"
+
+#echo "${first_row_string}"
 
 # Split that string into a new array called 'fields'
 # read -ra splits on whitespace (tabs and spaces)
-read -ra fields <<< "$first_row_string"
+#read -ra fields <<< "$first_row_string"
 
 # Now access an element from the 'fields' array (e.g., the 2nd field is at index 1)
-echo "${fields[1]}"
+#echo "${fields[1]}"
+
+
+# -------------------------------------------------------
+# ======================= Output ========================
+# -------------------------------------------------------
+
+clean_cnmops_del="${working_dir}/${batch}.DEL.${prefix}.bed.gz"
+clean_cnmops_del_index="${working_dir}/${batch}.DEL.${prefix}.bed.gz.tbi"
+clean_cnmops_dup="${working_dir}/${batch}.DUP.${prefix}.bed.gz"
+clean_cnmops_dup_index="${working_dir}/${batch}.DUP.${prefix}.bed.gz.tbi"
+
+del_filename="${output_dir}/$(basename "${clean_cnmops_del}")"
+del_index_filename="${output_dir}/$(basename "${clean_cnmops_del_index}")"
+dup_filename="${output_dir}/$(basename "${clean_cnmops_dup}")"
+dup_index_filename="${output_dir}/$(basename "${clean_cnmops_dup_index}")"
+
+mv "${clean_cnmops_del}" "${del_filename}"
+mv "${clean_cnmops_del_index}" "${del_index_filename}"
+mv "${clean_cnmops_dup}" "${dup_filename}"
+mv "${clean_cnmops_dup_index}" "${dup_index_filename}"
+
+outputs_json=$(jq -n \
+  --arg del "${del_filename}" \
+  --arg del_index "${del_index_filename}" \
+  --arg dup "${dup_filename}" \
+  --arg dup_index "${dup_index_filename}" \
+  '{Del: $del, Del_idx: $del_index, Dup: $dup, Dup_idx: $dup_index}' )
+echo "${outputs_json}" > "${output_json_filename}"
+
+echo "Finished cnmops successfully, output json filename: ${output_json_filename}"
