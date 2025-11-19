004ea12ab358576abfdc5c1a670c2e8139f03b3a
Mark Walker <markw@broadinstitute.org>
2025-11-19 10:08:18 -0500

    Remove svgenotyper prototype

--- src/svgenotyper/__init__.py
+++
@@ -1,2 +0,0 @@
-from .svgenotyper import train, genotype, cnv_train, cnv_infer
-from ._version import __version__
--- src/svgenotyper/svgenotyper/__init__.py
+++
@@ -1,2 +0,0 @@
-from .cnv_infer import main as cnv_infer
-from .cnv_train import main as cnv_train
