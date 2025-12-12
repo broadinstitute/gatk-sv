bcd1b6937eb415797fa48fd35202d78bef36d5cc
Vahid <VJalili@users.noreply.github.com>
2025-12-12 10:59:05 -0500

    Add gather sample evidence to single-sample pipeline.

--- src/sv_shell/single_sample_pipeline.sh
+++ src/sv_shell/single_sample_pipeline.sh
@@ -41,9 +41,57 @@ ref_panel_bincov_matrix=$(jq -r ".ref_panel_bincov_matrix" "$input_json")
 
 
 
+# -------------------------------------------------------
+# ======================= Command =======================
+# -------------------------------------------------------
+
+# GatherSampleEvidence
+# ---------------------------------------------------------------------------------------------------------------------
+
+gather_sample_evidence_wd=$(mktemp -d "/wd_GatherSampleEvidence_XXXXXXXX")
+gather_sample_evidence_wd="$(realpath ${gather_sample_evidence_wd})"
+gather_sample_evidence_inputs_json="${gather_sample_evidence_wd}/inputs.json"
+gather_sample_evidence_outputs_json="${gather_sample_evidence_wd}/outputs.json"
+
+jq -n \
+  --slurpfile inputs "${input_json}" \
+  '{
+    "sample_id": $inputs[0].sample_id,
+    "bam_or_cram_file": $inputs[0].bam_or_cram_file,
+    "bam_or_cram_index": $inputs[0].bam_or_cram_index,
+    "reference_dict": $inputs[0].reference_dict,
+    "reference_fasta": $inputs[0].reference_fasta,
+    "reference_index": $inputs[0].reference_index,
+    "primary_contigs_list": $inputs[0].primary_contigs_list,
+    "primary_contigs_fai": $inputs[0].primary_contigs_fai,
+    "preprocessed_intervals": $inputs[0].preprocessed_intervals,
+    "manta_region_bed": $inputs[0].manta_region_bed,
+    "manta_region_bed_index": $inputs[0].manta_region_bed_index,
+    "sd_locs_vcf": $inputs[0].sd_locs_vcf,
+    "mei_bed": $inputs[0].mei_bed,
+    "wham_include_list_bed_file": $inputs[0].wham_include_list_bed_file,
+    "reference_bwa_alt": $inputs[0].reference_bwa_alt,
+    "reference_bwa_amb": $inputs[0].reference_bwa_amb,
+    "reference_bwa_ann": $inputs[0].reference_bwa_ann,
+    "reference_bwa_bwt": $inputs[0].reference_bwa_bwt,
+    "reference_bwa_pac": $inputs[0].reference_bwa_pac,
+    "reference_bwa_sa": $inputs[0].reference_bwa_sa,
+    "collect_coverage": true,
+    "run_scramble": true,
+    "run_manta": true,
+    "run_wham": true,
+    "collect_pesr": true,
+    "scramble_alignment_score_cutoff": 90,
+    "run_module_metrics": $inputs[0].run_sampleevidence_metrics
+  }' > "${gather_sample_evidence_inputs_json}"
+
+bash /opt/sv_shell/gather_sample_evidence.sh \
+  "${gather_sample_evidence_inputs_json}" \
+  "${gather_sample_evidence_outputs_json}"
 
 
-# TEMP --------- pipelining
+
+# TODO: TEMP --------- pipelining
 # these inputs you get from gather sample evidence.
 # WDL: # File case_counts_file_ = select_first([case_counts_file, GatherSampleEvidence.coverage_counts])
 coverage_counts="/inputs/NA12878.counts.tsv.gz"
