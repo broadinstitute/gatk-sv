3bbdb4f86ae011e950b8ff8ec66fec9b67205141
xuefzhao <xuefzhao@umich.edu>
2026-01-21 23:10:05 -0500

    test

--- module18_annotate_genomic_context/annotate_genomic_context_helper.R
+++ module18_annotate_genomic_context/annotate_genomic_context_helper.R
@@ -22,7 +22,7 @@ safe_read_table <- function(file, ...) {
 }
 
 #integrate GC of each breakpoints:
-	dat=read.table(opt$input,sep='\t')
+	dat=safe_read_table(opt$input,sep='\t')
 	sr_le=safe_read_table(paste(opt$path,'il_inte.le_bp.vs.SR', sep='/'))
 	sd_le=safe_read_table(paste(opt$path,'il_inte.le_bp.vs.SD', sep='/'))
 	rm_le=safe_read_table(paste(opt$path,'il_inte.le_bp.vs.RM', sep='/'))
@@ -30,51 +30,65 @@ safe_read_table <- function(file, ...) {
 	sr_ri=safe_read_table(paste(opt$path,'il_inte.ri_bp.vs.SR', sep='/'))
 	sd_ri=safe_read_table(paste(opt$path,'il_inte.ri_bp.vs.SD', sep='/'))
 	rm_ri=safe_read_table(paste(opt$path,'il_inte.ri_bp.vs.RM', sep='/'))
-	dat[,ncol(dat)+1] = 'US'
-	if(nrow(rm_le)>0){
-			dat[dat[,4]%in%rm_le[,4],][,ncol(dat)]='RM'
-	}
-	if(nrow(rm_ri)>0){
-		dat[dat[,4]%in%rm_ri[,4],][,ncol(dat)]='RM'
-	}
-	if(nrow(sd_le)>0){
-		dat[dat[,4]%in%sd_le[,4],][,ncol(dat)]='SD'
-	}
-	if(nrow(sd_ri)>0){
-		dat[dat[,4]%in%sd_ri[,4],][,ncol(dat)]='SD'
-	}
-	if(nrow(sr_le)>0){
-		dat[dat[,4]%in%sr_le[,4],][,ncol(dat)]='SR'
-	}
-	if(nrow(sr_ri)>0){
-		dat[dat[,4]%in%sr_ri[,4],][,ncol(dat)]='SR'
-	}
+
+	if(nrow(dat)>0){
 
 
-	colnames(dat)[ncol(dat)] = 'GC'
-        colnames(dat)[4]='SVID'
+		dat[,ncol(dat)+1] = 'US'
+		if(nrow(rm_le)>0){
+				dat[dat[,4]%in%rm_le[,4],][,ncol(dat)]='RM'
+		}
+		if(nrow(rm_ri)>0){
+			dat[dat[,4]%in%rm_ri[,4],][,ncol(dat)]='RM'
+		}
+		if(nrow(sd_le)>0){
+			dat[dat[,4]%in%sd_le[,4],][,ncol(dat)]='SD'
+		}
+		if(nrow(sd_ri)>0){
+			dat[dat[,4]%in%sd_ri[,4],][,ncol(dat)]='SD'
+		}
+		if(nrow(sr_le)>0){
+			dat[dat[,4]%in%sr_le[,4],][,ncol(dat)]='SR'
+		}
+		if(nrow(sr_ri)>0){
+			dat[dat[,4]%in%sr_ri[,4],][,ncol(dat)]='SR'
+		}
+
+
+		colnames(dat)[ncol(dat)] = 'GC'
+	        colnames(dat)[4]='SVID'
  
-	if (file.exists(paste(opt$path,'il_inte.lg_cnv.vs.SR', sep='/'))) {
-		lg_cnv_sr=read.table(paste(opt$path,'il_inte.lg_cnv.vs.SR', sep='/'))
-		lg_cnv_sd=read.table(paste(opt$path,'il_inte.lg_cnv.vs.SD', sep='/'))
-		lg_cnv_rm=read.table(paste(opt$path,'il_inte.lg_cnv.vs.RM', sep='/'))
-		lg_cnv = cbind(lg_cnv_sr[,c(4,ncol(lg_cnv_sr))], lg_cnv_sd[,ncol(lg_cnv_sd)], lg_cnv_rm[,ncol(lg_cnv_rm)])
-		lg_cnv[,ncol(lg_cnv)+1] = 1-rowSums(lg_cnv[,c(2:4)])
-		colnames(lg_cnv)=c('SVID','SR','SD','RM','US')
-		lg_cnv[,ncol(lg_cnv)+1] = 'US'
-		if(nrow(lg_cnv[lg_cnv$RM>.5,])>0){
-			lg_cnv[lg_cnv$RM>.5,][,ncol(lg_cnv)]='RM'
-		}
-		if(nrow(lg_cnv[lg_cnv$SD>.5,])>0){
-			lg_cnv[lg_cnv$SD>.5,][,ncol(lg_cnv)]='SD'
-		}
-		if(nrow(lg_cnv[lg_cnv$SR>.5,])>0){
-			lg_cnv[lg_cnv$SR>.5,][,ncol(lg_cnv)]='SR'
-		}
+		if (file.exists(paste(opt$path,'il_inte.lg_cnv.vs.SR', sep='/'))) {
+			lg_cnv_sr=read.table(paste(opt$path,'il_inte.lg_cnv.vs.SR', sep='/'))
+			lg_cnv_sd=read.table(paste(opt$path,'il_inte.lg_cnv.vs.SD', sep='/'))
+			lg_cnv_rm=read.table(paste(opt$path,'il_inte.lg_cnv.vs.RM', sep='/'))
+			lg_cnv = cbind(lg_cnv_sr[,c(4,ncol(lg_cnv_sr))], lg_cnv_sd[,ncol(lg_cnv_sd)], lg_cnv_rm[,ncol(lg_cnv_rm)])
+			lg_cnv[,ncol(lg_cnv)+1] = 1-rowSums(lg_cnv[,c(2:4)])
+			colnames(lg_cnv)=c('SVID','SR','SD','RM','US')
+			lg_cnv[,ncol(lg_cnv)+1] = 'US'
+			if(nrow(lg_cnv[lg_cnv$RM>.5,])>0){
+				lg_cnv[lg_cnv$RM>.5,][,ncol(lg_cnv)]='RM'
+			}
+			if(nrow(lg_cnv[lg_cnv$SD>.5,])>0){
+				lg_cnv[lg_cnv$SD>.5,][,ncol(lg_cnv)]='SD'
+			}
+			if(nrow(lg_cnv[lg_cnv$SR>.5,])>0){
+				lg_cnv[lg_cnv$SR>.5,][,ncol(lg_cnv)]='SR'
+			}
 
- 
-		dat=merge(dat, lg_cnv[,c(1,6)], by='SVID',all=T)
-		dat[!is.na(dat[,ncol(dat)]),][,ncol(dat)-1] = dat[!is.na(dat[,ncol(dat)]),][,ncol(dat)] 
+	 
+			dat=merge(dat, lg_cnv[,c(1,6)], by='SVID',all=T)
+			dat[!is.na(dat[,ncol(dat)]),][,ncol(dat)-1] = dat[!is.na(dat[,ncol(dat)]),][,ncol(dat)] 
+		}
+	}
+
+	if(nrow(dat) == 0){
+		dat = data.frame('SVID' = 0, 'GC' = 0)
 	}
 	
 	write.table(dat[,c('SVID','GC')],opt$output, quote=F, sep='\t', col.names=T, row.names=F)
+
+
+
+
+
