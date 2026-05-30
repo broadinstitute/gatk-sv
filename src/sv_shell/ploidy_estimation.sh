b124782b650d3fd6f9522ada3edba87cf53b12e5
Vahid <VJalili@users.noreply.github.com>
2026-05-29 20:28:27 -0400

    Fix the moved script's path.

--- src/sv_shell/ploidy_estimation.sh
+++ src/sv_shell/ploidy_estimation.sh
@@ -61,7 +61,7 @@ zcat "${bincov_matrix}" \
 mkdir ploidy_est
 Rscript /opt/WGD/bin/estimatePloidy.R -z -O ./ploidy_est "${ploidy_matrix}"
 
-python /opt/sv-pipeline/02_evidence_assessment/estimated_CN_denoising.py \
+python /opt/sv-pipeline/pre_SVCalling_and_QC/estimated_CN_denoising.py \
   --binwise-copy-number ./ploidy_est/binwise_estimated_copy_numbers.bed.gz \
   --estimated-copy-number ./ploidy_est/estimated_copy_numbers.txt.gz \
   --output-stats cn_denoising_stats.tsv \
